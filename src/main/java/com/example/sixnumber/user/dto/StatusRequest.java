3101000397b1db64ca029f6474c1c11de620098c
hobakk <117063625+hobakk@users.noreply.github.com>
2023-05-07 01:14:36 +0900

    [fix] dto

--- src/main/java/com/example/sixnumber/user/dto/StatusRequest.java
+++ src/main/java/com/example/sixnumber/user/dto/StatusRequest.java
@@ -1,10 +1,8 @@
 package com.example.sixnumber.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class StatusRequest {
-	private final String msg;
+	private String msg;
 }
--- src/main/java/com/example/sixnumber/user/dto/WithdrawRequest.java
+++ src/main/java/com/example/sixnumber/user/dto/WithdrawRequest.java
@@ -1,10 +1,8 @@
 package com.example.sixnumber.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class WithdrawRequest {
-	private final String msg;
+	private String msg;
 }
