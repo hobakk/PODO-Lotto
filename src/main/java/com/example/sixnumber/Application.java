98fbf91688b9742da88b187a8f2bc7233948b1bb
hobakk <117063625+hobakk@users.noreply.github.com>
2023-05-07 02:26:54 +0900

    [feat] admin 생성

--- src/main/java/com/example/sixnumber/Application.java
+++ src/main/java/com/example/sixnumber/Application.java
@@ -1,7 +1,14 @@
 package com.example.sixnumber;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.sixnumber.user.dto.SignupRequest;
+import com.example.sixnumber.user.entity.User;
+import com.example.sixnumber.user.repository.UserRepository;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +17,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner dummyData(
+		UserRepository userRepository, PasswordEncoder passwordEncoder
+	) {
+		return args -> {
+
+			SignupRequest signupRequest = new SignupRequest("admin","asdf","관리자");
+			String password = passwordEncoder.encode(signupRequest.getPassword());
+			User admin = new User(signupRequest, password);
+			userRepository.save(admin);
+		};
+	}
 }
