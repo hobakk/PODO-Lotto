883c391ec079c94f2d91e1063209e96e0d2326c9
hobakk <117063625+hobakk@users.noreply.github.com>
2023-05-08 17:31:34 +0900

    [fix] 스캐줄러 수정 중

--- src/main/java/com/example/sixnumber/lotto/scheduler/LottoScheduler.java
+++ src/main/java/com/example/sixnumber/lotto/scheduler/LottoScheduler.java
@@ -1,12 +1,17 @@
 package com.example.sixnumber.lotto.scheduler;
 
 import java.text.SimpleDateFormat;
+import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.sixnumber.lotto.entity.Lotto;
+import com.example.sixnumber.lotto.entity.SixNumber;
 import com.example.sixnumber.lotto.repository.LottoRepository;
 import com.example.sixnumber.lotto.repository.SixNumberRepository;
 import com.example.sixnumber.lotto.service.LottoService;
@@ -24,15 +29,22 @@ public class LottoScheduler {
 
 	@Scheduled(cron = "0 0 0 0 * ?")
 	public void findByTopNumberListForMonth() {
-		// if (!lottoRepository.findBy()) {
-		//
-		// }
 
-		Date today = new Date();
-		SimpleDateFormat yd = new SimpleDateFormat("yyyy-MM");
-		String targetDate = yd.format(today);
-		String[] month = targetDate.split("-");
-		System.out.println(month[1]+"월 가장 많이 나온 숫자 top6");
-		// sixNumberRepository.
+		String ym = YearMonth.now().toString();
+		String[] yToM = ym.split("-");
+		int year = Integer.parseInt(yToM[0]);
+		int lastMonth = Integer.parseInt(yToM[1]) - 1;
+		YearMonth findYm = YearMonth.of(year, lastMonth);
+		List<Lotto> lottoList = lottoRepository.findByTopNubersForMonth(findYm);
+
+		if (lottoList.isEmpty()) {
+			System.out.println(lastMonth+"월 통계 생성중");
+			List<Integer> countList = new ArrayList<>();
+			for (int i = 0; i < 45; i++) {
+				countList.add(1);
+			}
+
+			List<SixNumber> monthDataList = sixNumberRepository.findAllByBuyDate(year, lastMonth);
+		}
 	}
 }
