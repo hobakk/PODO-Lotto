36c004ddd53acc78588290d4a16a6368df0e29b4
hobakk <117063625+hobakk@users.noreply.github.com>
2023-05-08 17:16:14 +0900

    [feat] Query문

--- src/main/java/com/example/sixnumber/lotto/repository/LottoRepository.java
+++ src/main/java/com/example/sixnumber/lotto/repository/LottoRepository.java
@@ -1,9 +1,16 @@
 package com.example.sixnumber.lotto.repository;
 
+import java.time.YearMonth;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sixnumber.lotto.entity.Lotto;
 
 public interface LottoRepository extends JpaRepository<Lotto, Long> {
 
+	@Query(value = "SELECT l FROM Lotto l WHERE l.creationDate = :yearMonth")
+	List<Lotto> findByTopNubersForMonth(@Param("yearMonth") YearMonth yearMonth);
 }
--- src/main/java/com/example/sixnumber/lotto/repository/SixNumberRepository.java
+++ src/main/java/com/example/sixnumber/lotto/repository/SixNumberRepository.java
@@ -1,12 +1,15 @@
 package com.example.sixnumber.lotto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sixnumber.lotto.entity.SixNumber;
 
 public interface SixNumberRepository extends JpaRepository<SixNumber, Long> {
-	// @Modifying
-	// @Query(value = "SELECT ")
+
+	@Query(value = "SELECT s FROM SixNumber s WHERE YEAR(s.buyDate) = :year AND MONTH(s.buyDate) = :month")
+	List<SixNumber> findAllByBuyDate(@Param("year") int year, @Param("month") int month);
 }
