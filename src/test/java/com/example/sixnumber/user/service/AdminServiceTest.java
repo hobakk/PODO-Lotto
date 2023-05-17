71b6e191c20b7d3d9986baca9f9e030da4868458
hobakk <117063625+hobakk@users.noreply.github.com>
2023-05-17 17:00:06 +0900

    [feat] AdminServiceTest exception test 추가 및 refactoring. 구현완료

--- src/test/java/com/example/sixnumber/user/service/AdminServiceTest.java
+++ src/test/java/com/example/sixnumber/user/service/AdminServiceTest.java
@@ -6,27 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import com.example.sixnumber.fixture.TestDataFactory;
 import com.example.sixnumber.global.dto.ApiResponse;
 import com.example.sixnumber.global.dto.ListApiResponse;
-import com.example.sixnumber.lotto.dto.LottoResponse;
 import com.example.sixnumber.lotto.entity.Lotto;
 import com.example.sixnumber.lotto.repository.LottoRepository;
 import com.example.sixnumber.user.dto.CashRequest;
+import com.example.sixnumber.user.dto.OnlyMsgRequest;
 import com.example.sixnumber.user.dto.UsersReponse;
 import com.example.sixnumber.user.entity.Cash;
 import com.example.sixnumber.user.entity.User;
 import com.example.sixnumber.user.repository.CashRepository;
 import com.example.sixnumber.user.repository.UserRepository;
-import com.example.sixnumber.user.type.UserRole;
+import com.example.sixnumber.user.type.Status;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminServiceTest {
@@ -43,34 +47,45 @@ public class AdminServiceTest {
 	@Mock
 	private RedisTemplate<String, String> redisTemplate;
 
+	private ValueOperations<String, String> valueOperations;
+	private User saveUser;
+	private User admin;
+	private Cash saveCash;
+
 	@BeforeEach
 	public void setup() {
 		// MockitoAnnotations.openMocks(this);
+		valueOperations = mock(ValueOperations.class);
+		saveUser = TestDataFactory.user();
+		admin = TestDataFactory.Admin();
+		saveCash = TestDataFactory.cash();
 	}
 
 	@Test
-	void setAdmin() {
-		User admin = mock(User.class);
-		when(admin.getId()).thenReturn(1L);
+	void setAdmin_success() {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn("AdminSecurityKey");
 
-		User target = mock(User.class);
-		when(target.getId()).thenReturn(99L);
-		when(target.getRole()).thenReturn(UserRole.ROLE_USER);
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
 
-		when(userRepository.findById(target.getId())).thenReturn(Optional.of(target));
+		ApiResponse response = adminService.setAdmin(request, admin, saveUser.getId());
 
-		ApiResponse response = adminService.setAdmin(admin, target.getId());
-
-		verify(target).setAdmin();
+		verify(userRepository).findById(anyLong());
 		assertEquals(response.getCode(), 200);
 		assertEquals(response.getMsg(), "변경 완료");
 	}
 
 	@Test
-	void getUsers() {
-		User user = TestDataFactory.user();
+	void setAdmin_fail_incorrectKey() {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn("false");
 
-		when(userRepository.findAll()).thenReturn(List.of(user));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.setAdmin(request, admin, saveUser.getId()));
+	}
+
+	@Test
+	void getUsers() {
+		when(userRepository.findAll()).thenReturn(List.of(saveUser));
 
 		ListApiResponse<UsersReponse> response = adminService.getUsers();
 
@@ -81,10 +96,9 @@ public class AdminServiceTest {
 
 	@Test
 	void getAfterChargs() {
-		Cash cash = TestDataFactory.cash();
-		cash.setProcessingAfter();
+		saveCash.setProcessingAfter();
 
-		when(cashRepository.processingEqaulAfter()).thenReturn(List.of(cash));
+		when(cashRepository.processingEqaulAfter()).thenReturn(List.of(saveCash));
 
 		ListApiResponse<Cash> response = adminService.getAfterChargs();
 
@@ -95,9 +109,7 @@ public class AdminServiceTest {
 
 	@Test
 	void getBeforeChargs() {
-		Cash cash = TestDataFactory.cash();
-
-		when(cashRepository.processingEqaulBefore()).thenReturn(List.of(cash));
+		when(cashRepository.processingEqaulBefore()).thenReturn(List.of(saveCash));
 
 		ListApiResponse<Cash> response = adminService.getBeforeChargs();
 
@@ -107,35 +119,42 @@ public class AdminServiceTest {
 	}
 
 	@Test
-	void upCash() {
+	void upCash_success() {
 		CashRequest request = TestDataFactory.cashRequest();
 
-		User user = mock(User.class);
-		when(user.getId()).thenReturn(7L);
-
-		Cash cash = mock(Cash.class);
-		when(cash.getUserId()).thenReturn(7L);
-
-		when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-		when(cashRepository.findById(anyLong())).thenReturn(Optional.of(cash));
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+		when(cashRepository.findById(anyLong())).thenReturn(Optional.of(saveCash));
 
 		ApiResponse response = adminService.upCash(request);
 
 		verify(userRepository).findById(anyLong());
 		verify(cashRepository).findById(anyLong());
-		verify(user).setCash("+", request.getValue());
-		verify(cash).setProcessingAfter();
 		assertEquals(response.getCode(), 200);
 		assertEquals(response.getMsg(), "충전 완료");
 	}
 
 	@Test
-	void downCash() {
+	void upCash_fail_incorrectUser() {
+		CashRequest request = mock(CashRequest.class);
+		when(request.getUserId()).thenReturn(4L);
+
+		Cash cash = mock(Cash.class);
+		when(cash.getUserId()).thenReturn(4L);
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+		when(cashRepository.findById(anyLong())).thenReturn(Optional.of(cash));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.upCash(request));
+
+		verify(userRepository).findById(anyLong());
+		verify(cashRepository).findById(anyLong());
+	}
+
+	@Test
+	void downCash_success() {
 		CashRequest request = TestDataFactory.cashRequest();
 
-		User user = TestDataFactory.user();
-
-		when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
 
 		ApiResponse response = adminService.downCash(request);
 
@@ -145,9 +164,21 @@ public class AdminServiceTest {
 	}
 
 	@Test
-	void createLottoWhenNoMain() {
-		Optional<Lotto> checkedLotto = Optional.empty();
-		when(lottoRepository.findByMain()).thenReturn(checkedLotto);
+	void downCash_fail_manyValue() {
+		CashRequest request = mock(CashRequest.class);
+		when(request.getValue()).thenReturn(10000);
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.downCash(request));
+
+		verify(userRepository).findById(anyLong());
+	}
+
+	@Test
+	void createLotto_success() {
+		Optional<Lotto> empty = Optional.empty();
+		when(lottoRepository.findByMain()).thenReturn(empty);
 
 		ApiResponse response = adminService.createLotto("email");
 
@@ -155,4 +186,76 @@ public class AdminServiceTest {
 		assertEquals(response.getCode(), 200);
 		assertEquals(response.getMsg(), "생성 완료");
 	}
+
+	@Test
+	void createLotto_fail_overlapLotto() {
+		Lotto lotto = mock(Lotto.class);
+
+		when(lottoRepository.findByMain()).thenReturn(Optional.of(lotto));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.createLotto(admin.getEmail()));
+
+		verify(lottoRepository).findByMain();
+	}
+
+	@Test
+	void setStatus_success_active() {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn("ACTIVE");
+
+		saveUser.setStatus("SUSPENDED");
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+
+		ApiResponse response = adminService.setStatus(admin, saveUser.getId(), request);
+
+		assertEquals(saveUser.getStatus(), Status.ACTIVE);
+		assertEquals(response.getCode(), 200);
+		assertEquals(response.getMsg(), "상태 변경 완료");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"SUSPENDED", "DORMANT"})
+	void setStatus_success_suspended_or_dormant(String statusStr) {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn(statusStr);
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+
+		// setStatus_success_active 에서 redisTemlate 값이 없을 때 검증되서 값이 있을 경우만 검증함
+		when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+		when(valueOperations.get(anyString())).thenReturn("RTV");
+
+		ApiResponse response = adminService.setStatus(admin, saveUser.getId(), request);
+
+		verify(redisTemplate).delete(anyString());
+		assertEquals(saveUser.getStatus(), Status.valueOf(statusStr));
+		assertEquals(response.getCode(), 200);
+		assertEquals(response.getMsg(), "상태 변경 완료");
+	}
+
+	@Test
+	void setStatus_fail_incorrectMsg() {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn("false");
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.setStatus(admin, saveUser.getId(), request));
+
+		verify(userRepository).findById(anyLong());
+	}
+
+	@Test
+	void setStatus_fail_overlapStatus() {
+		OnlyMsgRequest request = mock(OnlyMsgRequest.class);
+		when(request.getMsg()).thenReturn("ACTIVE");
+
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(saveUser));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> adminService.setStatus(admin, saveUser.getId(), request));
+
+		verify(userRepository).findById(anyLong());
+	}
+
 }
