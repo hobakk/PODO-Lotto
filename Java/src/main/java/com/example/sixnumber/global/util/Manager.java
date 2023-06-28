0578cef0796af730f1ad69234ab59f341ecc28de
hobakk <117063625+hobakk@users.noreply.github.com>
2023-06-28 21:18:17 +0900

    [fix] 전역 클래스, refactoring

---
+++ Java/src/main/java/com/example/sixnumber/global/util/Manager.java
@@ -0,0 +1,36 @@
+package com.example.sixnumber.global.util;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.example.sixnumber.global.exception.UserNotFoundException;
+import com.example.sixnumber.user.entity.User;
+import com.example.sixnumber.user.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class Manager {
+
+	private final UserRepository userRepository;
+
+	public User findUser(Object object) {
+		if (object instanceof Long userId) {
+			return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+		} else if (object instanceof String email) {
+			return userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+		} else throw new IllegalArgumentException("잘못된 접근입니다");
+	}
+
+	public String reviseResult(List<Integer> sortedIndices, List<Integer> countList) {
+		sortedIndices.sort((index1, index2) -> countList.get(index2).compareTo(countList.get(index1)));
+		List<Integer> topIndices = sortedIndices.subList(0, Math.min(sortedIndices.size(), 6));
+		Collections.sort(topIndices);
+		topIndices.replaceAll(Integer -> Integer + 1);
+		return topIndices.stream().map(Object::toString).collect(Collectors.joining(" "));
+	}
+}
