c6092e7b160bbb950112d5a7635570c27f93c153
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-01 02:41:45 +0900

    [feat] navigate menu

--- React/src/shared/Layout.js
+++ React/src/shared/Layout.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const CommonStyle = {
-    paddingLeft: `10px`,
-}
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from "styled-components"
 
 const HeaderStyles = {
-...CommonStyle,
+  margin: `0`,
   width: '100%',
-  background: 'black',
-  height: '50px',
+  background: `#D8B2D8`,
+  height: '60px',
   display: 'flex',
   alignItems: 'center',
+  justifyContent: 'center', // 중앙 배치
   color: 'white',
   fontWeight: '600',
+  fontSize: `22px`
 };
 
 const FooterStyles = {
   width: '100%',
   height: '50px',
   display: 'flex',
-  background: 'black',
+  background: '#D8B2D8',
   color: 'white',
   alignItems: 'center',
   justifyContent: 'center',
@@ -35,15 +34,79 @@ const layoutStyles = {
   minHeight: '90vh',
 }
 
+const navigationLinksStyles  = {
+  marginLeft: '20px',
+  display: 'flex',
+  alignItems: 'center',
+}
+
+const LiBox =styled.div`
+  display: inline-block;
+  margin-left: 10px;
+`
+
+function DropdownMenu() {
+  const [isDropdown, setDropdown] = useState(false);
+  const navigate = useNavigate();
+
+  const handleMouseEnter = () => {
+    setDropdown(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdown(false);
+  };
+
+  return (
+    <div
+      className="dropdown-menu"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ position: 'relative' }}
+    >
+      <div className="menu-trigger" style={{ cursor: 'pointer' }}>
+        <span>Menu</span>
+      </div>
+      {isDropdown && (
+        <div className="dropdown-content" style={{
+            position: 'absolute',
+            top: '100%',
+            left: '0',
+            backgroundColor: '#f2f2f2',
+            border: `1px solid red`,
+            padding: '10px',
+            zIndex: '1',
+          }}
+        >
+          <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+            <li>
+              <a href="/my-page">My Page</a>
+            </li>
+            <li>
+              <a href="/recommendation">Recommendation Number</a>
+            </li>
+            <li>
+              <a href="/statistics">Statistics</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
 function Header() {
     const navigate = useNavigate();
 
   return (
     <div style={{ ...HeaderStyles }}>
         <div id='LogoTitle' onClick={()=>{navigate("/lotto")}}>
-            <img src={process.env.PUBLIC_URL + `/logo.png`} alt='Logo' style={{ width: "30px", height: "30px" }} />
+            <img src={process.env.PUBLIC_URL + `/logo.png`} alt='Logo' style={{ width: "30px", height: "30px", }} />
             <span>포도 로또</span>
         </div>
+          <div className='navigation-links' style={navigationLinksStyles}>
+            <DropdownMenu />
+          </div>
     </div>
   );
 }
@@ -51,7 +114,8 @@ function Header() {
 function Footer() {
   return (
     <div style={{ ...FooterStyles }}>
-      <span>copyright @SCC</span>
+      <a href='https://github.com/hobakk/'>Github</a>
+      <a style={{paddingLeft: `20px`}} href='https://holloweyed-snail.tistory.com/'>Blog</a>
     </div>
   );
 }
