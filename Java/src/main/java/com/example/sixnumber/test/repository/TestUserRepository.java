7f25d939e60de1dac4bc8d59933ebf10a7c54911
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-05 00:19:54 +0900

    [delete] react test 를 마침

--- Java/src/main/java/com/example/sixnumber/test/repository/TestUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sixnumber.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sixnumber.test.entity.TestUser;
-
-public interface TestUserRepository extends JpaRepository<TestUser, Long> {
-
-}
