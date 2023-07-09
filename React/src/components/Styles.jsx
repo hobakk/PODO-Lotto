98df0a371c588b5a664a77ca8eb0ef1a50371b02
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-09 17:34:13 +0900

    [fix] style

--- React/src/components/Styles.jsx
+++ React/src/components/Styles.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
 const mainColor = `#9957F0`;
 
@@ -39,9 +40,10 @@ export const UlBox =styled.ul`
 `
 
 export const DividingLine =styled.div`
-  border-bottom: 1px solid black;
-  margin: 20px;
-  margin-bottom: 10px;
+  border-bottom: 1px solid gray;
+  margin: auto;
+  margin-bottom: 20px;
+  width: 4cm;
 `
 
 export const MenuTitle =styled.p`
@@ -55,11 +57,49 @@ export const Dropdown =styled.div`
   top: 100%;
   left: 0;
   display: flex;
-  background-color: black;
-  padding: 2px;
+  background-color: #9957F0;
+  padding: 5px;
   z-index: 1;
 `
 
 export const LiBox =styled.li`
   margin-bottom: 10px;
+`
+
+export const CustomLink =styled(Link)`
+  color: blue;
+  text-decoration: none;
+  font-size: 21px;
+`
+
+export const CommonLink = styled(Link)`
+  color: ${(props)=>props.color};
+  text-decoration: none;
+  margin-left: 20px;
+
+  &:hover {
+    color: blue;
+    cursor: pointer;
+  }
+
+  &:active {
+    color: red;
+    cursor: grabbing;
+  }
+`
+
+export const MenuDiv =styled.div`
+  cursor: pointer;
+  margin: auto;
+  margin-left: 80px;
+  margin-top: 8px;
+  width: 3cm;
+  height: 39px;
+  background-color: ${(props)=>props.color};
+  text-align: center;
+`
+
+export const MenuSpan =styled.span`
+  color: ${(props)=>props.color};
+  font-size: 28px;
 `
