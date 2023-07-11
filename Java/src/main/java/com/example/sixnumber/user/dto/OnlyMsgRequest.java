dc8a9dc8b0161735a6d78fa2a35cf4d6de985856
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-12 01:56:59 +0900

    [fix] response 값 추가

--- Java/src/main/java/com/example/sixnumber/user/dto/OnlyMsgRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/OnlyMsgRequest.java
@@ -1,8 +1,10 @@
 package com.example.sixnumber.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class OnlyMsgRequest {
 	private String msg;
 }
