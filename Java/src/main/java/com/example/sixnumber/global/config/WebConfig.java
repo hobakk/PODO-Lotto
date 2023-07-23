63affcaaf4ac5401b403cade66a5028392b8052d
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-24 02:04:25 +0900

    [fix] etc

--- Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
+++ Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:3000")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true)
 			.maxAge(1800);
