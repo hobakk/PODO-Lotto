abb749b79f2b27263e1ae7046d8591e60ff15dd8
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-27 22:43:05 +0900

    [fix] LottoController, Service Cacheable

--- Java/src/main/java/com/example/sixnumber/lotto/service/LottoService.java
+++ Java/src/main/java/com/example/sixnumber/lotto/service/LottoService.java
@@ -3,9 +3,11 @@ package com.example.sixnumber.lotto.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.sixnumber.global.dto.ItemApiResponse;
 import com.example.sixnumber.global.util.Manager;
 import com.example.sixnumber.lotto.dto.LottoResponse;
 import com.example.sixnumber.lotto.dto.YearMonthRequest;
@@ -24,6 +26,7 @@ public class LottoService {
 	private final LottoRepository lottoRepository;
 	private final Manager manager;
 
+	@Cacheable(cacheNames = "MainStats", key = "'all'")
 	public LottoResponse mainTopNumbers() {
 		Lotto lotto = lottoRepository.findByMain().orElseThrow(
 			() -> new IllegalArgumentException("해당 정보를 찾을 수 없습니다"));
@@ -33,6 +36,7 @@ public class LottoService {
 		return new LottoResponse(countList, result);
 	}
 
+	@Cacheable(value = "MonthStats", key = "#request.yearMonth")
 	public LottoResponse getTopNumberForMonth(YearMonthRequest request) {
 		Lotto lotto = lottoRepository.findByTopNumbersForMonth(request.getYearMonth())
 			.orElseThrow(() -> new IllegalArgumentException("해당 정보를 찾을 수 없습니다"));
