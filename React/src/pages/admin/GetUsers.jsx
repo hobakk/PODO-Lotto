cb473da62f28ff8279f684828035cbb7a7f9a5d5
hobakk <117063625+hobakk@users.noreply.github.com>
2023-07-31 02:24:17 +0900

    [fix] component 분리

--- React/src/pages/admin/GetUsers.jsx
+++ React/src/pages/admin/GetUsers.jsx
@@ -80,61 +80,47 @@ function GetUsers() {
     marginBottom: "5px"
   }
 
-  return (
+  const ResultContainer = ({ entityType }) => {
+    return (
+      <div key={entityType.id} style={MapFirstStyle}>
+        <div style={MapSecondStyle}>
+          <div style={{ display:"flex", padding: "10px" }}>
+            <span>id: {entityType.id}</span>
+            <span style={{ margin: "auto" }}>Cash: {entityType.cash}</span>
+            <input name={`${entityType.id}`} onChange={(e)=>onChangeHandler(e, entityType.id)} />
+            <button onClick={()=>onClickHandler(entityType.id)}>포인트 차감</button>
+          </div>
+          <div style={{ display:"flex", padding: "10px" }}>
+            <span>Email: {entityType.email}</span>
+            <span style={{ marginLeft: "auto" }}>Nickname: {entityType.nickname}</span>
+          </div>
+          <div style={{ display:"flex", padding: "10px" }}>
+            <span>Role: {entityType.role}</span>
+            <span style={{ marginLeft: "auto" }}>Status: {entityType.status}</span>
+          </div>
+        </div>  
+      </div>
+    )
+  }
 
+  return (
     <div id='recent' style={ CommonStyle }>
         <h1 style={{  fontSize: "80px", height:"1.5cm"}}>Get Users</h1>
         <input onChange={(e)=>setSearchInputValue(e.target.value)} placeholder='검색할 값을 입력해주세요' style={{ marginBottom:"1cm", width:"7cm", height:"0.5cm" }}/>
         {searchInputValue === "" ? (
             value.filter(user=>user.role !== "ROLE_ADMIN").map(user=>{
-              return (
-                <div key={user.id} style={MapFirstStyle}>
-                  <div style={MapSecondStyle}>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>id: {user.id}</span>
-                      <span style={{ margin: "auto" }}>Cash: {user.cash}</span>
-                      <input name={`${user.id}`} onChange={(e)=>onChangeHandler(e, user.id)} />
-                      <button onClick={()=>onClickHandler(user.id)}>포인트 차감</button>
-                    </div>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>Email: {user.email}</span>
-                      <span style={{ marginLeft: "auto" }}>Nickname: {user.nickname}</span>
-                    </div>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>Role: {user.role}</span>
-                      <span style={{ marginLeft: "auto" }}>Status: {user.status}</span>
-                    </div>
-                  </div>  
-                </div>
-              )
+              return <ResultContainer entityType={user} />
             })
         ):(
-          <div style={MapFirstStyle}>
+          <>
             {result.length === 0 ? (
               <div>검색 결과 없음</div>
             ):(
               result.map(item=>{
-                return(
-                  <div style={MapSecondStyle}>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>id: {item.id}</span>
-                      <span style={{ margin: "auto" }}>Cash: {item.cash}</span>
-                      <input name={`${item.id}`} onChange={(e)=>onChangeHandler(e, item.id)} />
-                      <button onClick={()=>onClickHandler(item.id)}>포인트 차감</button>
-                    </div>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>Email: {item.email}</span>
-                      <span style={{ marginLeft: "auto" }}>Nickname: {item.nickname}</span>
-                    </div>
-                    <div style={{ display:"flex", padding: "10px" }}>
-                      <span>Role: {item.role}</span>
-                      <span style={{ marginLeft: "auto" }}>Status: {item.status}</span>
-                    </div>
-                  </div>
-                )
+                return <ResultContainer entityType={item} />
               })
             )}
-          </div>
+          </>
         )}
     </div>
   )
