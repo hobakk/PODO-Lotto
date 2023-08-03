082d2701b13f4e93a5f7336806d112e6f7089f1b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-03 19:28:52 +0900

    [feat] test util

---
+++ Java/src/test/java/com/example/sixnumber/fixture/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.example.sixnumber.fixture;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return User.builder()
+			.username(username)
+			.password("password")
+			.roles("USER")
+			.build();
+	}
+}
