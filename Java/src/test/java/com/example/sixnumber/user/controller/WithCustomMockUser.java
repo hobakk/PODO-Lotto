0308c1995ccc988fce1fac05fbd8d80360fb6702
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-04 11:52:49 +0900

    [fix] @WithCustomMockUser

--- Java/src/test/java/com/example/sixnumber/user/controller/WithCustomMockUser.java
+++ Java/src/test/java/com/example/sixnumber/user/controller/WithCustomMockUser.java
@@ -5,9 +5,13 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.springframework.security.test.context.support.WithSecurityContext;
 
+import com.example.sixnumber.user.type.Status;
+import com.example.sixnumber.user.type.UserRole;
+
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
 public @interface WithCustomMockUser {
-	String role() default "ROLE_USER";
+	UserRole role() default UserRole.ROLE_USER;
 	String username() default "testUser";
+	Status status() default Status.ACTIVE;
 }
