56aa20cfa196dcd73cf41e7a1bb41d6f47a249b8
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-10 22:09:09 +0900

    [feat] TypeScript 초기 설정

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from 'react-cookie';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <CookiesProvider>
     <App />
-  </CookiesProvider>
+  </CookiesProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
