0e9f61edc215312ef505fe0add7dbb13e5c5b6a0
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-11 18:05:21 +0900

    [fix] configStore

--- React/src/config/configStore.ts
+++ React/src/config/configStore.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userIfReducer from "../modules/userIfSlice";
-import adminModeReducer from "../modules/adminMode";
 
 const store = configureStore({
     reducer: {
         userIf: userIfReducer,
-        adminMode: adminModeReducer,
     },
 });
 
