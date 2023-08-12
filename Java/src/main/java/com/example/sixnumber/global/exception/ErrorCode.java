c3940676ef90e41f0a8e3eca02c21fd183dbdc6a
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-12 18:05:24 +0900

    [feat] ErrorCode

--- Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
+++ Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ import lombok.Getter;
 public enum ErrorCode {
 	BREAK_THE_ROLE(HttpStatus.FORBIDDEN, "규정 위반으로 홈페이지를 이용할 수 없습니다"),
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다"),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다"),;
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다"),
+	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Expired Tokens, 만료된 토큰 입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String msg;
