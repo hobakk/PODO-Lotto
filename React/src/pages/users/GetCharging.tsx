cb692a469eb225cb86ecab6b73f8762c3e07dd4b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-17 00:55:47 +0900

    [fix] GetCharging style

--- React/src/pages/users/GetCharging.tsx
+++ React/src/pages/users/GetCharging.tsx
@@ -27,28 +27,46 @@ function GetCharging() {
     }
   }, [isAllow])
 
-  const outputBox: React.CSSProperties = {
-    border: "3px solid black",
-    width: "9cm",
-    height: "3cm",
-    fontSize: "22px",
-    marginTop: "5px",
+  const BoxStyle: React.CSSProperties = {
+    display:"flex",
+    justifyContent:"center",
+    textAlign:"center",
+    fontSize:"22px",
+    alignItems:"center",
+    flexDirection:'row',
+  }
+  const BorderStyle: React.CSSProperties = { 
+    border:"3px solid black",
+    borderRight:"0px",
+    borderBottom:"0px",
+    width:"9cm",
+    height:"1cm",
+    display: "flex", 
+    alignItems: "center", 
+    justifyContent: "center",
   }
 
   return (
-      <div style={ CommonStyle }>
-          <h1 style={{  fontSize: "80px" }}>getCharging</h1>
-          {chargValue.length !== 0 || chargValue !== null ? (
-            chargValue.map(item=>{
-              return (
-                <div key={item.msg} style={outputBox}>
-                  <p style={{ marginLeft: "20px"}}>msg: {item.msg}</p>
-                  <p style={{ marginLeft: "20px"}}>cash: {item.cash}</p>
-                </div>  
-              )
-            })
-          ):null}
-      </div>
+    <div style={ CommonStyle }>
+        <h1 style={{  fontSize: "80px" }}>getCharging</h1>
+        <div style={{ ...BoxStyle, marginTop:"1cm", }}>
+          <div style={{ ...BorderStyle, backgroundColor:"#D4F0F0", }}><span>MSG</span></div>
+          <div style={{ ...BorderStyle, width:"14cm", backgroundColor:"#D4F0F0", }}><span >CASH</span></div>
+          <div style={{ borderLeft:"3px solid black", height:"1.08cm" }} />
+        </div>
+        {chargValue.length !== 0 || chargValue !== null ? (
+          chargValue.map(item=>{
+            return (
+              <div style={BoxStyle}>
+                <div key={item.msg} style={BorderStyle}><span>{item.msg}</span></div>
+                <div key={item.msg} style={{ ...BorderStyle, width:"14cm" }}><span>{item.cash}</span></div>
+                <div style={{ borderLeft:"3px solid black", height:"1.08cm" }} />
+              </div>
+            )
+          })
+        ):null}
+        <div style={{ border:"3px solid black", borderBottom:"0px", width:"23cm", }}/>
+    </div>
   )
 }
 
