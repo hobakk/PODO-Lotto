963c27d67024a879b134b23cb748aa4cdb0476b1
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-16 18:01:11 +0900

    [fix] SetCharging useAllowLogin 적용

--- React/src/pages/users/SetCharging.tsx
+++ React/src/pages/users/SetCharging.tsx
@@ -3,11 +3,12 @@ import { InputBox, SignBorder, CommonStyle } from '../../components/Styles'
 import { setCharges } from '../../api/useUserApi';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { AllowLogin } from '../../components/CheckRole'
 import { Res, errorType } from '../../shared/TypeMenu';
+import { useAllowType } from '../../hooks/AllowType';
 
 function SetCharging() {
     const navigate = useNavigate();
+    useAllowType("AllowLogin");
     const [inputValue, setInputValue] = useState<{cash: number, msg: string}>({
         cash: 0,
         msg: "",
@@ -48,7 +49,6 @@ function SetCharging() {
 
   return (
     <div style={ SignBorder }>
-        <AllowLogin />
         <form onSubmit={submitHandler} style={ CommonStyle }>
             <h1 style={{  fontSize: "80px" }}>Charging</h1>
             <p>금액과 입금 메세지를 적어주세요</p>
