6fe843165c6c9b4e23cc2b577d10581708041d31
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-21 01:05:24 +0900

    [fix] LottoController 반환값

--- Java/src/main/java/com/example/sixnumber/lotto/controller/LottoController.java
+++ Java/src/main/java/com/example/sixnumber/lotto/controller/LottoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.sixnumber.global.dto.ItemApiResponse;
+import com.example.sixnumber.global.dto.UnifiedResponse;
 import com.example.sixnumber.lotto.dto.LottoResponse;
 import com.example.sixnumber.lotto.dto.YearMonthResponse;
 import com.example.sixnumber.lotto.service.LottoService;
@@ -23,17 +23,17 @@ public class LottoController {
 	private final LottoService lottoService;
 
 	@GetMapping("/main")
-	public ResponseEntity<ItemApiResponse<LottoResponse>> mainTopNumbers() {
-		return ResponseEntity.ok(ItemApiResponse.ok("조회 성공", lottoService.mainTopNumbers()));
+	public ResponseEntity<UnifiedResponse<LottoResponse>> mainTopNumbers() {
+		return ResponseEntity.ok(UnifiedResponse.ok("조회 성공", lottoService.mainTopNumbers()));
 	}
 
 	@GetMapping("/yearMonth")
-	public ResponseEntity<ItemApiResponse<LottoResponse>> getTopNumberForMonth(@RequestParam YearMonth yearMonth) {
-		return ResponseEntity.ok(ItemApiResponse.ok("조회 성공", lottoService.getTopNumberForMonth(yearMonth)));
+	public ResponseEntity<UnifiedResponse<LottoResponse>> getTopNumberForMonth(@RequestParam YearMonth yearMonth) {
+		return ResponseEntity.ok(UnifiedResponse.ok("조회 성공", lottoService.getTopNumberForMonth(yearMonth)));
 	}
 
 	@GetMapping("/yearMonth/all")
-	public ResponseEntity<ItemApiResponse<YearMonthResponse>> getAllMonthStats() {
-		return ResponseEntity.ok(ItemApiResponse.ok("조회 성공", lottoService.getAllMonthStats()));
+	public ResponseEntity<UnifiedResponse<YearMonthResponse>> getAllMonthStats() {
+		return ResponseEntity.ok(UnifiedResponse.ok("조회 성공", lottoService.getAllMonthStats()));
 	}
 }
