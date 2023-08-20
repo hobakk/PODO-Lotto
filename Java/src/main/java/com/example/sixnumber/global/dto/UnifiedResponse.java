28b2f8d7ceffa32415ec142f61351e88579a7308
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-21 01:43:19 +0900

    [refactor] UnifiedResponse

--- Java/src/main/java/com/example/sixnumber/global/dto/UnifiedResponse.java
+++ Java/src/main/java/com/example/sixnumber/global/dto/UnifiedResponse.java
@@ -2,38 +2,46 @@ package com.example.sixnumber.global.dto;
 
 import org.springframework.http.HttpStatus;
 
+import com.example.sixnumber.global.exception.CustomException;
+import com.example.sixnumber.global.exception.ErrorCode;
+
 import lombok.Getter;
 
 @Getter
 public class UnifiedResponse<T> {
 	private final int code;
-	private final String message;
+	private final String msg;
 	private T data;
 
-	public UnifiedResponse(int code, String message) {
+	private static final int OK = HttpStatus.OK.value();
+	private static final int CREATED = HttpStatus.CREATED.value();
+
+	public UnifiedResponse(int code, String msg) {
 		this.code = code;
-		this.message = message;
+		this.msg = msg;
 	}
 
-	public UnifiedResponse(int code, String message, T data) {
+	public UnifiedResponse(int code, String msg, T data) {
+		if (data == null) throw new CustomException(ErrorCode.MISSING_DATA);
+
 		this.code = code;
-		this.message = message;
+		this.msg = msg;
 		this.data = data;
 	}
 
-	public static <T> UnifiedResponse<T> ok(String message) {
-		return new UnifiedResponse<>(HttpStatus.OK.value(), message);
+	public static <T> UnifiedResponse<T> ok(String msg) {
+		return new UnifiedResponse<>(OK, msg);
 	}
 
-	public static <T> UnifiedResponse<T> ok(String message, T data) {
-		return new UnifiedResponse<>(HttpStatus.OK.value(), message, data);
+	public static <T> UnifiedResponse<T> ok(String msg, T data) {
+		return new UnifiedResponse<>(OK, msg, data);
 	}
 
-	public static <T> UnifiedResponse<T> create(String message) {
-		return new UnifiedResponse<>(HttpStatus.CREATED.value(), message);
+	public static <T> UnifiedResponse<T> create(String msg) {
+		return new UnifiedResponse<>(CREATED, msg);
 	}
 
 	public static <T> UnifiedResponse<T> create(String message, T data) {
-		return new UnifiedResponse<>(HttpStatus.CREATED.value(), message, data);
+		return new UnifiedResponse<>(CREATED, message, data);
 	}
 }
--- Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
+++ Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다"),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다"),
 	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "Expired Tokens, 만료된 토큰 입니다"),
-	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 RefreshToken 입니다");
+	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 RefreshToken 입니다"),
+	MISSING_DATA(HttpStatus.BAD_REQUEST, "UnifiedResponse data 가 누락되었습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
