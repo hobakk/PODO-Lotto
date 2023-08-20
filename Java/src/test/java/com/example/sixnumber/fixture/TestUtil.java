d7176546b65c0d847582a6b4d85c54a8d108b6f3
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-21 01:59:58 +0900

    [fix] etc

--- Java/src/test/java/com/example/sixnumber/fixture/TestUtil.java
+++ Java/src/test/java/com/example/sixnumber/fixture/TestUtil.java
@@ -8,18 +8,18 @@ import com.example.sixnumber.global.dto.UnifiedResponse;
 
 public class TestUtil {
 	public static <T> void UnifiedResponseEquals(UnifiedResponse<T> response, int code, String msg) {
-		assertEquals(response.getMessage(), msg);
+		assertEquals(response.getMsg(), msg);
 		assertEquals(response.getCode(), code);
 	}
 
 	public static <T> void UnifiedResponseEquals(UnifiedResponse<T> response, int code, String msg, Class<T> clazz) {
-		assertEquals(response.getMessage(), msg);
+		assertEquals(response.getMsg(), msg);
 		assertEquals(response.getCode(), code);
 		if (clazz != null) assertNotNull(response.getData());
 	}
 
 	public static <T> void UnifiedResponseListEquals(UnifiedResponse<List<T>> response, int code, String msg) {
-		assertEquals(response.getMessage(), msg);
+		assertEquals(response.getMsg(), msg);
 		assertEquals(response.getCode(), code);
 		assertNotNull(response.getData());
 	}
