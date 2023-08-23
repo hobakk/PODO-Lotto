b11ee9b08b06366d98250e593d4aba2fb5c1f871
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-23 16:01:25 +0900

    [fix] GetUserIfMutation useMutation

--- React/src/components/GetUserIfMutation.ts
+++ React/src/components/GetUserIfMutation.ts
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useMutation } from 'react-query';
 import { useDispatch } from 'react-redux';
-import { getInformation } from '../api/useUserApi';
+import { getInformation } from '../api/userApi';
 import { setUserIf } from '../modules/userIfSlice';
 import { UserIfState } from '../shared/TypeMenu';
+import { UnifiedResponse } from '../shared/TypeMenu';
 
 function GetUserIfMutation() {
     const dispatch = useDispatch();
 
-    const getIfMutation = useMutation(getInformation, {
-        onSuccess: (res: UserIfState)=>{
-            dispatch(setUserIf(res));
+    const getIfMutation = useMutation<UnifiedResponse<UserIfState>>(getInformation, {
+        onSuccess: (res)=>{
+            console.log(res)
+            if (res.code === 200 && res.data)
+            dispatch(setUserIf(res.data));
         }
     })
 
