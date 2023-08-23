54ca97b1d484ca6feb095870dcaaf609611ee694
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-23 16:02:14 +0900

    [ifx] lottoApi

--- React/src/api/lottoApi.ts
+++ React/src/api/lottoApi.ts
@@ -1,16 +1,20 @@
 import { UnifiedResponse } from "../shared/TypeMenu";
 import { api } from "./config";
 
-type LottoResponse = {
+export type LottoResponse = {
     countList: number[],
     value: string,
+}
+
+export type AllMonthProps = {
+    yearMonthList: string[],
 }
 
 export const getMainTopNumber = async (): Promise<UnifiedResponse<LottoResponse>> => {
     try {
         const { data } = await api.get("/lotto/main");
         return data;
-    } catch (error) {
+    } catch (error: any) {
         throw error;
     }
 }
@@ -23,16 +27,16 @@ export const getTopNumberForMonth = async (yearMonth: string): Promise<UnifiedRe
             }
         });
         return data;
-    } catch (error) {
+    } catch (error: any) {
         throw error;
     }
 }
 
-export const getAllMonthStats = async (): Promise<UnifiedResponse<{yearMonthList: string[]}>> => {
+export const getAllMonthStats = async (): Promise<UnifiedResponse<AllMonthProps>> => {
     try {
         const { data } = await api.get("/lotto/yearMonth/all");
         return data;   
-    } catch (error) {
+    } catch (error: any) {
         throw error;
     }
 }
