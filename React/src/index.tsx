7a3ec37832737e5b41eedbdab471db15c2ebc67b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-25 23:17:27 +0900

    [fix] index.jsx => .tsx

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from 'react-cookie';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <CookiesProvider>
     <App />
