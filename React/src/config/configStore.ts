eb4a4d04dc49062febb60d0301f35f65aa92c776
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-30 20:00:21 +0900

    [feat] redux persist transform encrypt 적용

--- React/src/config/configStore.ts
+++ React/src/config/configStore.ts
@@ -2,15 +2,27 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import userIfReducer from "../modules/userIfSlice";
 import { persistReducer, persistStore } from "redux-persist";
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+import { PersistConfig, Transform } from "redux-persist/es/types";
 
-const persistConfig= {
+const secretKey = process.env.REACT_APP_ENCRYPTION_KEY;
+
+const encryptor: Transform<any, any> = encryptTransform({
+  secretKey: secretKey!,
+  onError: (error)=>{
+    console.error("An error occurred:", error);
+  },
+});
+
+const persistConfig: PersistConfig<any>= {
     key: "root",
     storage,
+    transforms: [encryptor],
 };
 
 const rootReducer = combineReducers({
     userIf: userIfReducer,
-})
+});
 
 const persistRootReducer = persistReducer(persistConfig, rootReducer);
 
