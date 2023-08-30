3aa2e25ae73572d3d22e09615171e4c7d0762bf3
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-30 23:31:14 +0900

    [fix] JwtEntryPoint

--- Java/src/main/java/com/example/sixnumber/global/config/JwtEntryPoint.java
+++ Java/src/main/java/com/example/sixnumber/global/config/JwtEntryPoint.java
@@ -1,22 +1,37 @@
 package com.example.sixnumber.global.config;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.example.sixnumber.global.exception.CustomException;
+import com.example.sixnumber.global.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
+	private static final CustomException exception = new CustomException(ErrorCode.DONT_LOGIN);
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) throws IOException, ServletException {
 
-		response.sendError(HttpServletResponse.SC_FORBIDDEN, "권한이 없습니다.");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+		try (OutputStream os = response.getOutputStream()) {
+			ObjectMapper om = new ObjectMapper();
+			om.writeValue(os, exception);
+			os.flush();
+		}
 	}
 }
