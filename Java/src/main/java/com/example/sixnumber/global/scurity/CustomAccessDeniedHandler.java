bc60d55dd54aa49feabcea0fe994996d487ca932
hobakk <117063625+hobakk@users.noreply.github.com>
2023-08-31 21:20:59 +0900

    [feat] CustomAccessDeniedHandler

---
+++ Java/src/main/java/com/example/sixnumber/global/scurity/CustomAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package com.example.sixnumber.global.scurity;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.example.sixnumber.global.exception.CustomException;
+import com.example.sixnumber.global.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+	private static final CustomException exception = new CustomException(ErrorCode.BREAK_THE_ROLE);
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+		try (OutputStream os = response.getOutputStream()) {
+			ObjectMapper om = new ObjectMapper();
+			om.writeValue(os, exception);
+			os.flush();
+		}
+	}
+}
