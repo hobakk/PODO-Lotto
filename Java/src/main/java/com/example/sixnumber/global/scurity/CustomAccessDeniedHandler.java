733e8c65dc99ac498ea49b81d275fbc3252cee5b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-01 23:54:25 +0900

    [fix] CustomAccessDeniedHandler 특정 api 에 대한 처리

--- Java/src/main/java/com/example/sixnumber/global/scurity/CustomAccessDeniedHandler.java
+++ Java/src/main/java/com/example/sixnumber/global/scurity/CustomAccessDeniedHandler.java
@@ -10,23 +10,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 
 import com.example.sixnumber.global.dto.ExceptionDto;
 import com.example.sixnumber.global.exception.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.RequiredArgsConstructor;
-
 @Component
-@RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	private final ObjectMapper objectMapper;
-	private static final ExceptionDto exception = new ExceptionDto(ErrorCode.ACCESS_DENIED);
+	private final RequestMatcher requestMatcher;
+	private static ExceptionDto exception;
+
+	public CustomAccessDeniedHandler(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+		this.requestMatcher = request -> request.getRequestURI().startsWith("/api/admin/");
+	}
 
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		if (requestMatcher.matches(request)) exception = new ExceptionDto(ErrorCode.ONLY_ADMIN_ACCESS_API);
+ 		else exception = new ExceptionDto(ErrorCode.ACCESS_DENIED);
 
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
 		response.setStatus(HttpStatus.FORBIDDEN.value());
