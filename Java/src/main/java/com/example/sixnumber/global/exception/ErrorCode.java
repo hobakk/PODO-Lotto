11cc4064e26ce14ff31fa91e64a88d9b48b68fcd
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-01 23:54:04 +0900

    [fix] ErrorCode

--- Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
+++ Java/src/main/java/com/example/sixnumber/global/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
 	MISSING_DATA(HttpStatus.BAD_REQUEST, "UnifiedResponse data 가 누락되었습니다."),
 	NO_MATCHING_INFO_FOUND(HttpStatus.BAD_REQUEST, "일치하는 정보를 찾을 수 없습니다"),
 	DONT_LOGIN(HttpStatus.UNAUTHORIZED, "로그인 후 이용해주세요"),
-	ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 리소스에 대한 권한이 없습니다");
+	ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 리소스에 대한 권한이 없습니다"),
+	ONLY_ADMIN_ACCESS_API(HttpStatus.FORBIDDEN, "관리자만 접근 가는한 API 입니다");
 
 	private final HttpStatus httpStatus;
 	private final String message;
