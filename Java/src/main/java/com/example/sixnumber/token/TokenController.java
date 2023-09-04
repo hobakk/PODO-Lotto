6c3a7693ba4b32b308a6104888401f3c975888a5
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-04 15:19:19 +0900

    #51 [fix] TokenController

--- Java/src/main/java/com/example/sixnumber/token/TokenController.java
+++ Java/src/main/java/com/example/sixnumber/token/TokenController.java
@@ -1,37 +1,36 @@
-// package com.example.sixnumber.token;
-//
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-//
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// import com.example.sixnumber.global.dto.UnifiedResponse;
-// import com.example.sixnumber.user.dto.MyInformationResponse;
-//
-// import lombok.RequiredArgsConstructor;
-//
-// @RestController
-// @RequiredArgsConstructor
-// @RequestMapping("/api/jwt")
-// public class TokenController {
-// 	private final TokenService tokenService;
-//
-// 	@PostMapping("/check/login")
-// 	public ResponseEntity<UnifiedResponse<MyInformationResponse>> getInformationAfterCheckLogin(
-// 		HttpServletRequest request, HttpServletResponse response
-// 	) {
-// 		UserIfAndCookieResponse userIfAndCookieResponse = tokenService.getInformationAfterCheckLogin(request);
-// 		if (userIfAndCookieResponse.getCookie() != null) response.addCookie(userIfAndCookieResponse.getCookie());
-//
-// 		return ResponseEntity.ok(UnifiedResponse.ok("조회 및 재발급 성공", userIfAndCookieResponse.getResponse()));
-// 	}
-//
-// 	@PostMapping("/renew/access")
-// 	public ResponseEntity<UnifiedResponse<?>> renewAccessToken(@RequestBody String refreshToken, HttpServletResponse response) {
-// 		response.addCookie(tokenService.renewAccessToken(refreshToken));
-// 		return ResponseEntity.ok(UnifiedResponse.ok("AccessToken 재발급 성공"));
-// 	}
-// }
+package com.example.sixnumber.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sixnumber.global.dto.UnifiedResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/jwt")
+public class TokenController {
+	private final TokenService tokenService;
+
+	@PostMapping("/re-issuance")
+	public ResponseEntity<UnifiedResponse<?>> reIssuance(
+		@RequestBody ReIssuanceRequest request, HttpServletResponse response
+	) {
+		Cookie accessCookie = tokenService.reIssuance(request);
+		response.addCookie(accessCookie);
+		return ResponseEntity.ok(UnifiedResponse.ok("AccessToken 재발급 성공"));
+	}
+
+	// @PostMapping("/renew/access")
+	// public ResponseEntity<UnifiedResponse<?>> renewAccessToken(@RequestBody String refreshToken, HttpServletResponse response) {
+	// 	response.addCookie(tokenService.renewAccessToken(refreshToken));
+	// 	return ResponseEntity.ok(UnifiedResponse.ok("AccessToken 재발급 성공"));
+	// }
+}
