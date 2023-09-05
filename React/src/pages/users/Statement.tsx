fa947c4747a2193a1e5dd1610ee7876ef2037379
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-05 11:52:27 +0900

    [fix] Statement.tsx

--- React/src/pages/users/Statement.tsx
+++ React/src/pages/users/Statement.tsx
@@ -5,13 +5,11 @@ import { useMutation } from 'react-query'
 import { UnifiedResponse, Err } from '../../shared/TypeMenu';
 
 function Statement() {
-    const [isAssign, setAssign] = useState<boolean>(false);
     const [value, setValue] = useState<{localDate: string, msg: string}[]>([]);
 
     const StateMnetMutation = useMutation<UnifiedResponse<{localDate: string, msg: string}[]>, Err>(getStatement, {
         onSuccess: (res)=>{
             if (res.code === 200 && res.data) {
-                setAssign(true);
                 setValue(res.data);
             }
         },
@@ -21,10 +19,9 @@ function Statement() {
     });
 
     useEffect(()=>{
-        if  (isAssign === false) {
-            StateMnetMutation.mutate();
-        }
-    }, [isAssign])
+        if  (value.length === 0)
+        StateMnetMutation.mutate();
+    }, [])
 
     const PStyle: React.CSSProperties = {
         border: "2px solid black",
@@ -39,9 +36,9 @@ function Statement() {
   return (
     <div style={ CommonStyle }>
         <h1 style={{  fontSize: "80px" }}>Statement</h1>
-        {isAssign && (
-            <div>
-                {value.map((item, index)=>{
+        <div>
+            {value.length !== 0 && (
+                value.map((item, index)=>{
                     return (
                         <div key={item.localDate + index} style={{ fontSize: "20px"}}>
                             <div style={{ display: "flex"}}>
@@ -50,12 +47,12 @@ function Statement() {
                                     <p style={{ marginLeft: "15px" }}>{item.msg}</p>
                                 </span>
                             </div>
-                        </div>    
+                        </div>   
                     )
-                })}
-                <div style={{ borderTop: "2px solid black", width: "24cm"}} />
-            </div>
-        )}
+                })
+            )}
+            <div style={{ borderTop: "2px solid black", width: "24cm"}} />
+        </div>
     </div>
   )
 }
