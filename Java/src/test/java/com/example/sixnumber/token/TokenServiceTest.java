92bde7c23082ed29e853901008c4c02a48e5963e
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-05 16:20:36 +0900

    [feat] TokenServiceTest reIssuance_success

--- Java/src/test/java/com/example/sixnumber/token/TokenServiceTest.java
+++ Java/src/test/java/com/example/sixnumber/token/TokenServiceTest.java
@@ -1,112 +1,71 @@
-// package com.example.sixnumber.token;
-//
-// import static org.junit.jupiter.api.Assertions.*;
-// import static org.mockito.ArgumentMatchers.*;
-// import static org.mockito.Mockito.*;
-//
-// import javax.servlet.http.HttpServletRequest;
-//
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.mockito.junit.jupiter.MockitoExtension;
-//
-// import com.example.sixnumber.fixture.TestDataFactory;
-// import com.example.sixnumber.global.exception.CustomException;
-// import com.example.sixnumber.global.util.JwtProvider;
-// import com.example.sixnumber.global.util.Manager;
-// import com.example.sixnumber.user.dto.CookiesResponse;
-// import com.example.sixnumber.user.entity.User;
-//
-// import io.jsonwebtoken.Claims;
-// import io.jsonwebtoken.ExpiredJwtException;
-//
-// @ExtendWith(MockitoExtension.class)
-// public class TokenServiceTest {
-// 	@InjectMocks
-// 	private TokenService tokenService;
-//
-// 	@Mock
-// 	private JwtProvider jwtProvider;
-// 	@Mock
-// 	private Manager manager;
-//
-// 	private User saveUser;
-// 	private HttpServletRequest request;
-// 	private CookiesResponse cookies;
-//
-// 	@BeforeEach
-// 	public void setup() {
-// 		saveUser = TestDataFactory.user();
-// 		request = mock(HttpServletRequest.class);
-// 		cookies = TestDataFactory.cookiesResponse();
-// 	}
-//
-// 	@Test
-// 	void getInformationAfterCheckLogin_ValidAccessToken() {
-// 		when(jwtProvider.getTokenValueInCookie(request)).thenReturn(cookies);
-//
-// 		when(jwtProvider.validateToken(anyString())).thenReturn(true);
-// 		when(jwtProvider.getTokenInUserId(anyString())).thenReturn(saveUser.getId());
-//
-// 		when(manager.findUser(anyLong())).thenReturn(saveUser);
-//
-// 		UserIfAndCookieResponse response = tokenService.getInformationAfterCheckLogin(request);
-//
-// 		verify(jwtProvider).getTokenValueInCookie(request);
-// 		verify(jwtProvider).validateToken(anyString());
-// 		verify(jwtProvider).getTokenInUserId(anyString());
-// 		assertNull(response.getCookie());
-// 		assertNotNull(response.getResponse());
-// 	}
-//
-// 	@Test
-// 	void getInformationAfterCheckLogin_InvalidAccessToken() {
-// 		Claims claims = mock(Claims.class);
-// 		when(claims.getSubject()).thenReturn("refreshTokenPointer");
-//
-// 		when(jwtProvider.getTokenValueInCookie(request)).thenReturn(cookies);
-// 		when(jwtProvider.validateToken(anyString())).thenReturn(false);
-// 		when(jwtProvider.isTokenExpired(anyString())).thenReturn(false);
-// 		when(jwtProvider.getClaims(anyString())).thenReturn(claims);
-// 		when(jwtProvider.accessToken(claims.getSubject())).thenReturn(cookies.getAccessCookie().getValue());
-// 		when(jwtProvider.createCookie(anyString(), anyString())).thenReturn(cookies.getAccessCookie());
-//
-// 		when(manager.findUser(anyLong())).thenReturn(saveUser);
-//
-// 		UserIfAndCookieResponse response = tokenService.getInformationAfterCheckLogin(request);
-//
-// 		verify(jwtProvider).validateToken(anyString());
-// 		verify(jwtProvider).isTokenExpired(anyString());
-// 		verify(jwtProvider).getClaims(anyString());
-// 		verify(jwtProvider).accessToken(anyString());
-// 		verify(jwtProvider).createCookie(anyString(), anyString());
-// 		verify(manager).findUser(anyLong());
-// 		assertNotNull(response.getResponse());
-// 		assertNotNull(response.getCookie());
-// 	}
-//
-// 	@Test
-// 	void getInformationAfterCheckLogin_Fail_CookiesIsNull() {
-// 		when(jwtProvider.getTokenValueInCookie(request)).thenReturn(new CookiesResponse());
-//
-// 		Assertions.assertThrows(CustomException.class, ()->tokenService.getInformationAfterCheckLogin(request));
-//
-// 		verify(jwtProvider).getTokenValueInCookie(request);
-// 	}
-//
-// 	@Test
-// 	void getInformationAfterCheckLogin_fail_ExpiredJwtException() {
-// 		when(jwtProvider.getTokenValueInCookie(request)).thenReturn(cookies);
-// 		when(jwtProvider.validateToken(cookies.getAccessCookie().getValue())).thenReturn(false);
-// 		when(jwtProvider.isTokenExpired(cookies.getRefreshCookie().getValue())).thenThrow(ExpiredJwtException.class);
-//
-// 		Assertions.assertThrows(CustomException.class,
-// 			() -> tokenService.getInformationAfterCheckLogin(request));
-//
-// 		verify(jwtProvider).isTokenExpired(anyString());
-// 	}
-// }
+package com.example.sixnumber.token;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import javax.servlet.http.Cookie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.sixnumber.fixture.TestDataFactory;
+import com.example.sixnumber.global.util.JwtProvider;
+import com.example.sixnumber.global.util.Manager;
+import com.example.sixnumber.global.util.RedisDao;
+import com.example.sixnumber.user.entity.User;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenServiceTest {
+	@InjectMocks
+	private TokenService tokenService;
+
+	@Mock
+	private JwtProvider jwtProvider;
+	@Mock
+	private Manager manager;
+	@Mock
+	private PasswordEncoder passwordEncoder;
+	@Mock
+	private RedisDao redisDao;
+
+	private User saveUser;
+	private Cookie cookie;
+	private ReIssuanceRequest request;
+
+	@BeforeEach
+	public void setup() {
+		saveUser = TestDataFactory.user();
+		cookie = new Cookie("accessToken", "tokenValue");
+		request = new ReIssuanceRequest(saveUser.getId(), saveUser.getEmail(), "tokenValue");
+	}
+
+	@Test
+	void reIssuance_success() {
+		when(manager.findUser(anyString())).thenReturn(saveUser);
+
+		when(redisDao.getValue(anyString())).thenReturn(cookie.getValue());
+
+		when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
+
+		when(jwtProvider.validateRefreshToken(anyString())).thenReturn(true);
+		when(jwtProvider.accessToken(anyString())).thenReturn(cookie.getValue());
+		when(jwtProvider.getRemainingTime(anyString())).thenReturn(3600000L);
+		when(jwtProvider.createCookie(anyString(), anyString(), anyLong())).thenReturn(cookie);
+
+		Cookie response = tokenService.reIssuance(request);
+
+		verify(manager).findUser(anyString());
+		verify(redisDao).getValue(anyString());
+		verify(passwordEncoder).matches(anyString(), anyString());
+		verify(jwtProvider).validateRefreshToken(anyString());
+		verify(jwtProvider).accessToken(anyString());
+		verify(jwtProvider).getRemainingTime(anyString());
+		assertNotNull(response);
+	}
+}
