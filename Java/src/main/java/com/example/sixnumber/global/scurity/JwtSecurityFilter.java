c25c631d3ae6878577a0a4da14272796e753002b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-07 23:34:22 +0900

    #51 [refactor] JwtSecurityFilter

--- Java/src/main/java/com/example/sixnumber/global/scurity/JwtSecurityFilter.java
+++ Java/src/main/java/com/example/sixnumber/global/scurity/JwtSecurityFilter.java
@@ -14,12 +14,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.example.sixnumber.global.exception.AccessTokenIsExpiredException;
+import com.example.sixnumber.global.exception.RefreshTokenIsNullException;
 import com.example.sixnumber.global.util.JwtProvider;
 import com.example.sixnumber.global.util.RedisDao;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -27,33 +32,67 @@ public class JwtSecurityFilter extends OncePerRequestFilter {
 	private final UserDetailsServiceImpl userDetailsService;
 	private final JwtProvider jwtProvider;
 	private final RedisDao redisDao;
+	private final PasswordEncoder encoder;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		String accessToken = jwtProvider.getAccessTokenInCookie(request);
+		String encodedRefreshToken = jwtProvider.resolveToken(request);
 
-		if (accessToken != null) {
-			if (!jwtProvider.validateToken(accessToken)) {
-				throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "잘못된 AccessToken 입니다");
+		if (encodedRefreshToken != null) {
+			try {
+				validateAccessToken(accessToken);
+			} catch (ExpiredJwtException e) {
+				String refreshPointer = e.getClaims().getSubject();
+				String refreshToken = redisDao.getValue(refreshPointer);
+				if (!encoder.matches(refreshToken, encodedRefreshToken)) deleteCookie(response);
+
+				Claims claims = jwtProvider.getClaims(refreshToken);
+				String newAccessToken = jwtProvider.accessToken(claims.get("key", String.class));
+				long remainingSeconds = Math.max(jwtProvider.getRemainingTime(refreshToken) /1000, 0);
+				if (remainingSeconds < 360) deleteCookie(response);
+
+				Cookie cookie = jwtProvider.createCookie(JwtProvider.ACCESS_TOKEN, newAccessToken, remainingSeconds);
+				response.addCookie(cookie);
+				createAuthentication(claims.get("id", Long.class));
 			}
+		} else {
+			if (accessToken != null) {
+				try {
+					String verifiedAccessToken = validateAccessToken(accessToken);
+					String refreshPointer = jwtProvider.getClaims(verifiedAccessToken).getSubject();
+					String refreshToken = redisDao.getValue(refreshPointer);
+					if (refreshToken == null) {
+						redisDao.setBlackList(verifiedAccessToken);
+						deleteCookies(response);
+						throw new RefreshTokenIsNullException();
+					}
 
-			if (redisDao.isEqualsBlackList(accessToken)) {
-				throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 로그아웃된 AccessToken 입니다");
+					createAuthentication(jwtProvider.getTokenInUserId(refreshToken));
+				} catch (ExpiredJwtException e) {
+					throw new AccessTokenIsExpiredException();
+				}
 			}
-
-			String refreshPointer = jwtProvider.getClaims(accessToken).getSubject();
-			String refreshToken = redisDao.getValue(refreshPointer);
-			if (refreshToken == null) {
-				redisDao.setBlackList(accessToken);
-				deleteCookies(response);
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "RefreshToken is different");
-			}
-
-			createAuthentication(jwtProvider.getTokenInUserId(refreshToken));
 		}
 
 		filterChain.doFilter(request, response);
+	}
+
+	private String validateAccessToken(String accessToken) {
+		if (!jwtProvider.validateToken(accessToken)) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "잘못된 AccessToken 입니다");
+		}
+		if (redisDao.isEqualsBlackList(accessToken)) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 로그아웃된 AccessToken 입니다");
+		}
+
+		return accessToken;
+	}
+
+	private void deleteCookie(HttpServletResponse response) {
+		deleteCookies(response);
+		throw new RefreshTokenIsNullException();
 	}
 
 	private void deleteCookies(HttpServletResponse response) {
