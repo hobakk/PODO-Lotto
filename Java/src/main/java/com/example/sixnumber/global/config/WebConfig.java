2a0296795ed78920df9072768df9600e73735201
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-08 01:08:51 +0900

    [fix] WebConfig exposedHeaders 특정 headers 읽기 활성화

--- Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
+++ Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedOrigins("http://localhost:3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 			.allowedHeaders("*")
+			.exposedHeaders("Authorization")
 			.allowCredentials(true);
 	}
 }
