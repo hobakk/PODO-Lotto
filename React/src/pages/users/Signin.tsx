b602e7bba89d2587edb5a4acd73740238d0379ab
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-07 19:58:20 +0900

    [fix] Signin.tsx

--- React/src/pages/users/Signin.tsx
+++ React/src/pages/users/Signin.tsx
@@ -6,19 +6,15 @@ import { signin, SigninRequest } from '../../api/noneUserApi';
 import { InputBox } from '../../components/Styles';
 import GetUserIfMutation from '../../components/GetUserIfMutation';
 import { Err, UnifiedResponse } from '../../shared/TypeMenu';
-import { useDispatch } from 'react-redux';
-import { setRefreshToken } from '../../modules/refreshTokenSlice';
 
 function Signin() {
     const emailRef = useRef<HTMLInputElement>(null);
-    const dispatch = useDispatch();
     const navigate = useNavigate();
     const getUserIfMutation = GetUserIfMutation();
 
     const signinMutation = useMutation<UnifiedResponse<undefined>, Err, SigninRequest>(signin,{
         onSuccess: (res)=>{
-            if  (res.code === 200 && res.data) {
-                dispatch(setRefreshToken(res.data));
+            if  (res.code === 200) {
                 getUserIfMutation.mutate();
                 navigate("/");
             }
@@ -52,28 +48,39 @@ function Signin() {
     }, [])
 
   return (
-    <div style={ SignBorder }>
-        <div style={ CommonStyle }>
-            <h3 style={{ fontSize: "80px"}}>Login</h3>
-            <form onSubmit={submitHandler} style={{ fontSize: "30px", display: "flex", flexDirection: "column", }}>
-                <div style={{ marginBottom: "15px"}}>
-                    Email: <InputBox type='text' name="email" ref={emailRef} placeholder='test@email.com' onChange={onChangeHandler} style={{ marginLeft: "56px"}}></InputBox>
-                </div>
-                <div>
-                    Password: <InputBox type='password' name='password' placeholder='********' onChange={onChangeHandler} style={{marginBottom: "2cm"}}></InputBox>
-                </div>
-                <div style={{ fontSize: "18px", margin: "auto", marginBottom: "1cm"}}>
-                    <div>
-                        <Link to="/incorrect">비밀번호를 잊으셨나요 ?</Link>
-                    </div>
-                    <div>
-                        <Link to="/signup">비회원 이신가요 ?</Link>
-                    </div>
-                </div>
-                <div style={{marginLeft: "auto"}}>
-                    <button style={{ width: "100px", height: "25px" }}>로그인</button>
-                </div>
-            </form>
+    <div style={ CommonStyle }>
+        <h3 style={{ fontSize: "80px"}}>Login</h3>
+        <form onSubmit={submitHandler} style={{ fontSize: "30px" }}>
+            <div style={{ marginBottom: "30px", width: "15cm", display: "flex",}}>
+                <span style={{ margin: "0", }}>Email:</span>
+                <InputBox 
+                    type='text' 
+                    name="email" 
+                    ref={emailRef} 
+                    placeholder='test@email.com'
+                    autoComplete='email' 
+                    onChange={onChangeHandler} 
+                    style={{ marginLeft:"auto", textAlign: "center", }}
+                />
+            </div>
+            <div style={{ marginBottom: "30px", width: "15cm", display: "flex",}}>
+                <span style={{ margin: "0", }}>Password:</span>
+                <InputBox 
+                    type='password' 
+                    name='password' 
+                    placeholder='**************' 
+                    autoComplete='password' 
+                    onChange={onChangeHandler} 
+                    style={{ marginLeft:"auto", textAlign: "center", }}
+                />
+            </div>
+            <div style={{ marginLeft: "auto", width: "7.2cm" }}>
+                <button style={{ width: "100%", height: "30px" }}>로그인</button>
+            </div>
+        </form>
+        <div style={{ display:"flex", flexDirection:"column", fontSize: "18px", marginRight:"auto", marginTop:"30px" }}>
+            <Link to="/incorrect" style={{ marginBottom:"20px" }}>비밀번호를 잊으셨나요 ?</Link>
+            <Link to="/signup">비회원 이신가요 ?</Link>
         </div>
     </div>
   )
