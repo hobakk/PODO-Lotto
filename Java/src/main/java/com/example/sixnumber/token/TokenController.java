ea41dacbeeb22ef54d070fac86e1dc3562a29d76
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-08 14:08:32 +0900

    [fix] TokenController, Service, test 주석처리

--- Java/src/main/java/com/example/sixnumber/token/TokenController.java
+++ Java/src/main/java/com/example/sixnumber/token/TokenController.java
@@ -1,30 +1,30 @@
-package com.example.sixnumber.token;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.sixnumber.global.dto.UnifiedResponse;
-
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/jwt")
-public class TokenController {
-	private final TokenService tokenService;
-
-	@PostMapping("/re-issuance")
-	public ResponseEntity<UnifiedResponse<?>> reIssuance(
-		@RequestBody ReIssuanceRequest request, HttpServletResponse response
-	) {
-		Cookie accessCookie = tokenService.reIssuance(request);
-		response.addCookie(accessCookie);
-		return ResponseEntity.ok(UnifiedResponse.ok("AccessToken 재발급 성공"));
-	}
-}
+// package com.example.sixnumber.token;
+//
+// import javax.servlet.http.Cookie;
+// import javax.servlet.http.HttpServletResponse;
+//
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import com.example.sixnumber.global.dto.UnifiedResponse;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/api/jwt")
+// public class TokenController {
+// 	private final TokenService tokenService;
+//
+// 	@PostMapping("/re-issuance")
+// 	public ResponseEntity<UnifiedResponse<?>> reIssuance(
+// 		@RequestBody ReIssuanceRequest request, HttpServletResponse response
+// 	) {
+// 		Cookie accessCookie = tokenService.reIssuance(request);
+// 		response.addCookie(accessCookie);
+// 		return ResponseEntity.ok(UnifiedResponse.ok("AccessToken 재발급 성공"));
+// 	}
+// }
