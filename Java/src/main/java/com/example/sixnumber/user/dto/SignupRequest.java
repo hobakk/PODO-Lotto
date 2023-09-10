8763f89256c26a3e68fc4029f5a06075d245645d
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-10 21:49:45 +0900

    [fix] SignupRequest @Pattern 추가

--- Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.example.sixnumber.user.dto;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SignupRequest {
+	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "올바른 이메일 주소 형식이어야 합니다.")
 	private String email;
+
+	@Pattern(regexp = "^[A-Za-z0-9~!@#$%^&*=,.?]{8,60}$", message = "대소문자, 숫자, 특수문자 범주 안에 최소 8자 최대 60자")
 	private String password;
+
+	@Pattern(regexp = "^[A-Za-z0-9가-힣+_]{2,10}$", message = "대소문자, 숫자, 한글 범주 안에 최소 2자 최대 10자")
 	private String nickname;
 }
