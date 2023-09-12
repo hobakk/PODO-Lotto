23d06763472f02db938a0d59fbeb32575ef30573
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-12 22:55:37 +0900

    [refactor] CustomAuthenticationSuccessHandler

--- Java/src/main/java/com/example/sixnumber/global/scurity/CustomAuthenticationSuccessHandler.java
+++ Java/src/main/java/com/example/sixnumber/global/scurity/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,6 @@
 package com.example.sixnumber.global.scurity;
 
 import java.io.IOException;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
@@ -14,8 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+import com.example.sixnumber.global.dto.TokenDto;
 import com.example.sixnumber.global.util.JwtProvider;
+import com.example.sixnumber.global.util.Manager;
 import com.example.sixnumber.global.util.RedisDao;
+import com.example.sixnumber.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +26,7 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 	private final PasswordEncoder encoder;
 	private final JwtProvider jwtProvider;
 	private final RedisDao redisDao;
+	private final Manager manager;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -31,19 +34,19 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
 		OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 		String email = oAuth2User.getAttribute("email");
-		Long userId = oAuth2User.getAttribute("userId");
+		User user = manager.findUser(email);
 
-		String refreshPointer = UUID.randomUUID().toString();
-		String accessToken = jwtProvider.accessToken(refreshPointer);
-		String refreshToken = jwtProvider.refreshToken(email, userId, refreshPointer);
-		redisDao.setRefreshToken(refreshPointer, refreshToken, (long) 7, TimeUnit.DAYS);
+		TokenDto tokenDto = jwtProvider.generateTokens(user);
+		redisDao.setRefreshToken(tokenDto.getRefreshPointer(), tokenDto.getRefreshToken(), (long) 7, TimeUnit.DAYS);
 
-		Cookie accessCookie = jwtProvider.createCookie(JwtProvider.ACCESS_TOKEN, accessToken, "oneWeek");
-		String encodedRefreshToken = encoder.encode(refreshToken);
+		Cookie accessCookie = jwtProvider.createCookie(JwtProvider.ACCESS_TOKEN, tokenDto.getAccessToken(), "oneWeek");
+		String encodedRefreshToken = encoder.encode(tokenDto.getRefreshToken());
+		Cookie JsessionId = jwtProvider.createCookie("JSESSIONID", null, 0);
 
 		response.addCookie(accessCookie);
 		response.addHeader(JwtProvider.AUTHORIZATION_HEADER, "Bearer " + encodedRefreshToken);
+		response.addCookie(JsessionId);
 
-		getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000/");
+		getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000/oauth2/user");
 	}
 }
