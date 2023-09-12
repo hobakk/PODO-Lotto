eed669be8d9442df4a065a4b48d4575132a13738
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-12 22:02:56 +0900

    [fix] TokenDto

--- Java/src/main/java/com/example/sixnumber/global/dto/TokenDto.java
+++ Java/src/main/java/com/example/sixnumber/global/dto/TokenDto.java
@@ -1,15 +1,26 @@
 package com.example.sixnumber.global.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class TokenDto {
 	private String accessToken;
 	private String refreshToken;
+	private String refreshPointer;
+
+	public TokenDto(String accessToken, String refreshToken) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.refreshPointer = null;
+	}
+
+	public TokenDto(String accessToken, String refreshToken, String refreshPointer) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.refreshPointer = refreshPointer;
+	}
 }
