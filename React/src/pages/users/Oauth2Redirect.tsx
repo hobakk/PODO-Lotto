000a479a9787d53b1a5b0c7e23ef9acbcc094084
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-13 14:02:57 +0900

    [fix] Oauth2Redirect.tsx

--- React/src/pages/users/Oauth2Redirect.tsx
+++ React/src/pages/users/Oauth2Redirect.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect } from 'react'
-import GetUserIfMutation from '../../components/GetUserIfMutation'
 import { useNavigate } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import { getUserIfAndRefreshToken } from '../../api/userApi';
+import { useDispatch } from 'react-redux';
+import { setUserIf } from '../../modules/userIfSlice';
+import { Err } from '../../shared/TypeMenu';
 
 function Oauth2Redirect() {
-    const getUserIfMutation = GetUserIfMutation();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();  
+  const navigate = useNavigate();
 
-    useEffect(()=>{ 
-        getUserIfMutation.mutate(); 
-        navigate("/");
-    }, [])
+  const getUserIfAndRefreshMutation = useMutation(getUserIfAndRefreshToken, {
+    onSuccess: (res)=>{
+      if (res.code === 200 && res.data) {
+          dispatch(setUserIf(res.data));
+      }
+    },
+    onError: (err: Err)=>{
+      alert(err.msg);
+    }
+  });
+
+  useEffect(()=>{ 
+    getUserIfAndRefreshMutation.mutate(); 
+    navigate("/");
+  }, [])
 
   return null;
 }
