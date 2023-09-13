66982ccb9316e4c19bd575779f199bbda7008dda
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-13 14:05:43 +0900

    [fix] OAuthAttributes kakao, naver 추가

--- Java/src/main/java/com/example/sixnumber/global/util/OAuthAttributes.java
+++ Java/src/main/java/com/example/sixnumber/global/util/OAuthAttributes.java
@@ -26,9 +26,16 @@ public class OAuthAttributes {
 	}
 
 	public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-		// 여기서 네이버와 카카오 등 구분 (ofNaver, ofKakao)
-
-		return ofGoogle(userNameAttributeName, attributes);
+		switch (registrationId) {
+			case "google":
+				return ofGoogle(userNameAttributeName, attributes);
+			case "kakao":
+				return ofKakao("email", attributes);
+			case "naver":
+				return ofNaver("id", attributes);
+			default:
+				throw new RuntimeException();
+		}
 	}
 
 	private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -38,6 +45,31 @@ public class OAuthAttributes {
 			.picture((String) attributes.get("picture"))
 			.attributes(attributes)
 			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
+	private static OAuthAttributes ofKakao(String attributeKey, Map<String, Object> attributes) {
+		Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+		Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+		return OAuthAttributes.builder()
+			.name((String) kakaoProfile.get("nickname"))
+			.email((String) kakaoAccount.get("email"))
+			.picture((String) kakaoProfile.get("profile_image_url"))
+			.attributes(attributes)
+			.nameAttributeKey(attributeKey)
+			.build();
+	}
+
+	private static OAuthAttributes ofNaver(String attributeKey, Map<String, Object> attributes) {
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+		return OAuthAttributes.builder()
+			.name((String) response.get("name"))
+			.email((String) response.get("email"))
+			.picture((String) response.get("profile_image"))
+			.attributes(attributes)
+			.nameAttributeKey(attributeKey)
 			.build();
 	}
 
