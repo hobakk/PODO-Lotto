6aaf46e83591b837530f168e4da961fc7077c20f
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-14 17:51:31 +0900

    [fix] OAuthAttributes

--- Java/src/main/java/com/example/sixnumber/global/util/OAuthAttributes.java
+++ Java/src/main/java/com/example/sixnumber/global/util/OAuthAttributes.java
@@ -1,5 +1,6 @@
 package com.example.sixnumber.global.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.example.sixnumber.user.entity.User;
@@ -25,30 +26,26 @@ public class OAuthAttributes {
 		this.picture = picture;
 	}
 
-	public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+	public static OAuthAttributes of(String registrationId, String nameAttributeKey,  Map<String, Object> attributes){
 		switch (registrationId) {
-			case "google":
-				return ofGoogle(userNameAttributeName, attributes);
-			case "kakao":
-				return ofKakao("email", attributes);
-			case "naver":
-				return ofNaver("id", attributes);
-			default:
-				throw new RuntimeException();
+			case "google": return ofGoogle(nameAttributeKey, attributes);
+			case "kakao": return ofKakao("nickname", attributes);
+			case "naver": return ofNaver("id", attributes);
+			default: throw new RuntimeException();
 		}
 	}
 
-	private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+	private static OAuthAttributes ofGoogle(String nameAttributeKey, Map<String, Object> attributes) {
 		return OAuthAttributes.builder()
 			.name((String) attributes.get("name"))
 			.email((String) attributes.get("email"))
 			.picture((String) attributes.get("picture"))
 			.attributes(attributes)
-			.nameAttributeKey(userNameAttributeName)
+			.nameAttributeKey(nameAttributeKey)
 			.build();
 	}
 
-	private static OAuthAttributes ofKakao(String attributeKey, Map<String, Object> attributes) {
+	private static OAuthAttributes ofKakao(String nameAttributeKey, Map<String, Object> attributes) {
 		Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
 		Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
 
@@ -57,11 +54,11 @@ public class OAuthAttributes {
 			.email((String) kakaoAccount.get("email"))
 			.picture((String) kakaoProfile.get("profile_image_url"))
 			.attributes(attributes)
-			.nameAttributeKey(attributeKey)
+			.nameAttributeKey(nameAttributeKey)
 			.build();
 	}
 
-	private static OAuthAttributes ofNaver(String attributeKey, Map<String, Object> attributes) {
+	private static OAuthAttributes ofNaver(String nameAttributeKey, Map<String, Object> attributes) {
 		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
 		return OAuthAttributes.builder()
@@ -69,11 +66,19 @@ public class OAuthAttributes {
 			.email((String) response.get("email"))
 			.picture((String) response.get("profile_image"))
 			.attributes(attributes)
-			.nameAttributeKey(attributeKey)
+			.nameAttributeKey(nameAttributeKey)
 			.build();
 	}
 
 	public User toEntity(){
 		return new User(email, name, UserRole.ROLE_USER);
+	}
+
+	Map<String, Object> convertToMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", nameAttributeKey);
+		map.put("sub", nameAttributeKey);
+		map.put("email", email);
+		return map;
 	}
 }
