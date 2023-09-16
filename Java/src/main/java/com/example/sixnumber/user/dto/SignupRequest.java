f275accd221ae5576a280a46cb0a7631d8edb4f9
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-16 12:58:53 +0900

    [fix] SignupRequest msg

--- Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
@@ -12,12 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SignupRequest {
-	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "올바른 이메일 주소 형식이어야 합니다.")
+	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+		message = "올바른 이메일 주소 형식이어야 합니다")
 	private String email;
 
-	@Pattern(regexp = "^[A-Za-z0-9~!@#$%^&*=,.?]{8,60}$", message = "대소문자, 숫자, 특수문자 범주 안에 최소 8자 최대 60자")
+	@Pattern(regexp = "^[A-Za-z0-9~!@#$%^&*=,.?]{8,60}$",
+		message = "비밀번호는 대소문자, 숫자, 특수문자 범주 안에 최소 8자 최대 60자 형식에 맞게 작성해주세요")
 	private String password;
 
-	@Pattern(regexp = "^[A-Za-z0-9가-힣+_]{2,10}$", message = "대소문자, 숫자, 한글 범주 안에 최소 2자 최대 10자")
+	@Pattern(regexp = "^[A-Za-z0-9가-힣+_]{2,10}$",
+		message = "닉네임은 대소문자, 숫자, 한글 범주 안에 최소 2자 최대 10자 형식에 맞게 작성해주세요")
 	private String nickname;
 }
