6f49a6992262d2ed5d8a60d84b648114de07f74b
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-16 13:22:31 +0900

    [fix] SignupRequest @Email, @NotBlank

--- Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.example.sixnumber.user.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
@@ -12,14 +14,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SignupRequest {
-	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
-		message = "올바른 이메일 주소 형식이어야 합니다")
+	@NotBlank(message = "이메일 주소를 입력하세요.")
+	@Email(message = "올바른 이메일 주소 형식이어야 합니다")
 	private String email;
 
+	@NotBlank(message = "비밀번호를 입력하세요.")
 	@Pattern(regexp = "^[A-Za-z0-9~!@#$%^&*=,.?]{8,60}$",
 		message = "비밀번호는 대소문자, 숫자, 특수문자 범주 안에 최소 8자 최대 60자 형식에 맞게 작성해주세요")
 	private String password;
 
+	@NotBlank(message = "닉네임을 입력하세요.")
 	@Pattern(regexp = "^[A-Za-z0-9가-힣+_]{2,10}$",
 		message = "닉네임은 대소문자, 숫자, 한글 범주 안에 최소 2자 최대 10자 형식에 맞게 작성해주세요")
 	private String nickname;
