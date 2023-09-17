020f46aaf8e4ac2d3512ad9ca218ea0fa6970c8c
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-17 20:05:15 +0900

    [fix] Premium.tsx

--- React/src/pages/users/Premium.tsx
+++ React/src/pages/users/Premium.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { SignBorder, CommonStyle } from '../../components/Styles'
+import { SignBorder, CommonStyle, InputBoxStyle } from '../../components/Styles'
 import { getCashNickname, setPaid, CashNicknameDto } from '../../api/userApi'
 import { useMutation } from 'react-query'
 import { useDispatch, useSelector } from 'react-redux'
@@ -16,10 +16,10 @@ function Premium() {
 
     const borderDiv: React.CSSProperties = {
         border: "3px solid black",
-        width: "9cm",
-        height:"10cm",
+        width: "14cm",
+        height:"18cm",
+        padding:"50px",
         backgroundColor: "yellow",
-        textAlign: "center",
         fontSize: "18px",
     }
 
@@ -70,22 +70,37 @@ function Premium() {
   return (
     <div style={ CommonStyle }>
         {userRole === "ROLE_PAID" ? (
-            <div>
-                <h1 style={{  fontSize: "80px" }}>Release</h1>
-                <div style={borderDiv}>
+            <div style={borderDiv}>
+                <div style={{ height:"80%" }}>
+                    <h1 style={{ fontSize: "80px", textAlign:"center" }}>Release</h1>
                     <p>{userIf.nickname} 님은 Premium 등급입니다</p>
                     <p>등급 변경은 매월 초에 업데이트됩니다</p>
-                    <button onClick={()=>{setUserMutation.mutate("월정액 해지")}} style={{ marginTop: "5cm" }}>프리미엄 해제하기</button>
-                </div>  
-            </div>
+                    <p>프리미엄을 해제 신청 후 납기일 기준 31일 경과전까지 유지됩니다</p>
+                </div>
+                <div style={{ textAlign:"center" }}>
+                    <button 
+                        onClick={() => { setUserMutation.mutate("월정액 해지") }}
+                        style={{ width: "7cm", height:"1cm"}}
+                    >
+                        프리미엄 해제하기
+                    </button>
+                </div>
+            </div>  
         ):(
-            <div>
-                <h1 style={{  fontSize: "80px" }}>Premium</h1>
-                <div style={borderDiv}>
+            <div style={borderDiv}>
+                <div style={{ height:"80%" }}>
+                    <h1 style={{ fontSize: "80px", textAlign:"center" }}>Premium</h1>
                     <p>매월 5000원 차감</p>
                     <p>통계 서비스 이용 가능</p>
-                    <button onClick={(onClikcHandler)} style={{ marginTop: "5cm" }}>프리미엄 이용하기</button>
-                </div>  
+                </div> 
+                <div style={{ textAlign:"center" }}>
+                    <button 
+                        onClick={onClikcHandler}
+                        style={{ width: "7cm", height:"1cm"}}
+                    >
+                        프리미엄 이용하기
+                    </button>
+                </div>
             </div>    
         )}
     </div>
