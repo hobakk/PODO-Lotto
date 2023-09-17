d0d71e9ffcb786747a55b09ea7b7c986dceee058
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-17 23:15:59 +0900

    [fix] SetWinNumber.tsx

--- React/src/pages/admin/SetWinNumber.tsx
+++ React/src/pages/admin/SetWinNumber.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { CommonStyle } from '../../components/Styles'
+import { ButtonDiv, ButtonStyle, CommonStyle, InputBox, MsgAndInput } from '../../components/Styles'
 import { useMutation } from 'react-query';
 import { WinNumberRequest, setWinNumber } from '../../api/adminApi';
 import { UnifiedResponse, Err } from '../../shared/TypeMenu';
@@ -24,12 +24,6 @@ function SetWinNumber() {
             else if (err.code) alert(err.msg);
         }
     })
-
-    const InputStyle: React.CSSProperties = {
-        width: "7cm",
-        height: "0.8cm",
-        marginBottom: "15px",
-    }
 
     const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -57,13 +51,30 @@ function SetWinNumber() {
 
   return (
     <form style={ CommonStyle } onSubmit={onSubmitHandler}>
-        <h1 style={{  fontSize: "80px" }}>Set WinNumber</h1>
-        <input style={InputStyle} name='date' onChange={onChangeHandler} ref={dateRef} placeholder='date : 0000-00-00' />
-        <input style={InputStyle} name='time' onChange={onChangeHandler} placeholder='time : 0000' />
-        <input style={InputStyle} name='prize' onChange={onChangeHandler} placeholder='prize : 0000000000' />
-        <input style={InputStyle} name='winner' onChange={onChangeHandler} placeholder='winner : 0' />
-        <input style={InputStyle} name='numbers' onChange={onChangeHandler} placeholder='number : 1 2 3 4 5 6' />
-        <button style={InputStyle}>등록하기</button>
+        <h1 style={{ fontSize: "80px" }}>Set WinNumber</h1>
+        <div style={MsgAndInput}>
+            <span>Date:</span>
+            <InputBox name='date' onChange={onChangeHandler} ref={dateRef} placeholder='2023-07-14' />
+        </div>
+        <div style={MsgAndInput}>
+            <span>Time:</span>
+            <InputBox name='time' onChange={onChangeHandler} placeholder='1075' />
+        </div>
+        <div style={MsgAndInput}>
+            <span>Prize:</span>
+            <InputBox name='prize'  onChange={onChangeHandler} placeholder='1000000000' />
+        </div>
+        <div style={MsgAndInput}>
+            <span>Winner:</span>
+            <InputBox name='winner' onChange={onChangeHandler} placeholder='9' />
+        </div>
+        <div style={MsgAndInput}>
+            <span>Numbers:</span>
+            <InputBox name='numbers' onChange={onChangeHandler} placeholder='10 12 14 16 42 43' />
+        </div>
+        <div style={ButtonDiv}>
+            <button style={ButtonStyle}>등록하기</button>
+        </div>
     </form>
   )
 }
