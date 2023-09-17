be6dd1b4ea98cda9e70f12dd378df460c64ecdce
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-17 20:12:50 +0900

    [del] noneUserApi.tx checkLoginAndgetUserIf

--- React/src/api/noneUserApi.ts
+++ React/src/api/noneUserApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { api } from "./config";
-import { UnifiedResponse, Err, UserIfState, WinNumber } from "../shared/TypeMenu";
+import { UnifiedResponse, WinNumber } from "../shared/TypeMenu";
 
 export type SigninRequest = {
     email: string,
@@ -18,7 +18,7 @@ const signin = async (emailPassword: SigninRequest): Promise<UnifiedResponse<und
         const response = await api.post(`/users/signin`, emailPassword);    
         return response.data;
     } catch (error: any) {
-        throw error.response.data;
+        throw error.data;
     }
 }
 
@@ -36,26 +36,8 @@ const getWinNumber = async (): Promise<UnifiedResponse<{winNumberList: WinNumber
         const res = await axios.get(`${process.env.REACT_APP_SPRING_URL}/winnumber`);
         return res.data;  
     } catch (error: any) {
-        const err: Err = error.response.data;
-        throw err;
+        throw error.response.data;
     }
 }
 
-const checkLoginAndgetUserIf = async (): Promise<UnifiedResponse<UserIfState>> => {
-    try {
-        const res = await axios.post(`${process.env.REACT_APP_SPRING_URL}/jwt/check/login`, {
-            withCredentials: true,
-        });
-        console.log(res.data);
-        return res.data;
-    } catch (error: any) {
-        if (error.response.data.message ===
-            "JWT signature does not match locally computed signature. JWT validity cannot be asserted and should not be trusted.") {
-            throw new Error("SignatureException");
-        } else {
-            throw error.response.data;
-        }
-    }
-}
-
-export { signin, signup, getWinNumber, checkLoginAndgetUserIf}
+export { signin, signup, getWinNumber, }
