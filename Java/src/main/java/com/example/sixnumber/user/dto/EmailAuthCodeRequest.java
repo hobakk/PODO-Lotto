4d6497342dd65c672d9ba008342a9c4c00927052
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-22 16:56:42 +0900

    [fix] EmailAuthCodeRequest 기본 생성자

--- Java/src/main/java/com/example/sixnumber/user/dto/EmailAuthCodeRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/EmailAuthCodeRequest.java
@@ -1,10 +1,10 @@
 package com.example.sixnumber.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class EmailAuthCodeRequest {
 	private String email;
 	private String authCode;
