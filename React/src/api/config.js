a68457680c880cc0f002ec3b95d104c640977040
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-22 16:59:42 +0900

    [fix] config.js dontLogin axios

--- React/src/api/config.js
+++ React/src/api/config.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const url = `${process.env.REACT_APP_SPRING_URL}`
 
-// const signApi = axios.create({
-//     headers: {
-//         'Access-Control-Allow-Origin': 'http://localhost:8080'	// 서버 domain, httpOnly 설정 때문에 추가
-//     },
-//     baseURL: `${url}/users`,
-//     withCredentials: true,
-// })
+const dontLogin = axios.create({
+    headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:8080',
+        "Content-Type": 'application/json',	// 서버 domain, httpOnly 설정 때문에 추가
+    },
+    baseURL: url,
+    withCredentials: true,
+})
 
 const api = axios.create({
     headers: {
@@ -18,4 +19,4 @@ const api = axios.create({
     withCredentials: true,
 })
 
-export { api };
+export { dontLogin, api };
