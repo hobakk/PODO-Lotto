6ce8fd437c147010968bddd7ac52c05d51330f47
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-26 23:20:34 +0900

    #54 [fix] StatementResponse

--- Java/src/main/java/com/example/sixnumber/user/dto/StatementResponse.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/StatementResponse.java
@@ -1,15 +1,22 @@
 package com.example.sixnumber.user.dto;
 
+import com.example.sixnumber.user.entity.Statement;
+
 import lombok.Getter;
 
 @Getter
 public class StatementResponse {
+	private final String subject;
 	private final String localDate;
+	private final int cash;
 	private final String msg;
+	private final boolean modify;
 
-	public StatementResponse(String sentence) {
-		String[] value = sentence.split(",");
-		this.localDate = value[0];
-		this.msg = value[1];
+	public StatementResponse(Statement statement) {
+		this.subject = statement.getSubject();
+		this.localDate = statement.getLocalDate().toString();
+		this.cash = statement.getCash();
+		this.msg = statement.getMsg();
+		this.modify = statement.isModify();
 	}
 }
