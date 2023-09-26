2e7a50dd843da42053fced01f52b5cf4ae9376c7
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-26 18:16:23 +0900

    [feat] GetCharge.tsx deleteChargeMutation 및 처리

--- React/src/pages/users/GetCharge.tsx
+++ React/src/pages/users/GetCharge.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { CommonStyle } from '../../shared/Styles'
-import { ChargeResponse, getCharges } from '../../api/userApi'
+import { ButtonDiv, ButtonStyle, CommonStyle } from '../../shared/Styles'
+import { ChargeResponse, deleteCharge, getCharges } from '../../api/userApi'
 import { useMutation } from 'react-query'
 import { UnifiedResponse, Err } from '../../shared/TypeMenu';
 
 function GetCharge() {
-  const [chargValue, setChargValue] = useState<ChargeResponse>({
+  const initialData = {
     msg: "",
     cash: 0,
     date: "",
+  };
+
+  const [chargValue, setChargValue] = useState<ChargeResponse>({
+    ...initialData
   });
 
   const { msg, cash, date } = chargValue;
@@ -22,45 +26,63 @@ function GetCharge() {
     onError: (err: any)=>{
       if  (err.status) console.log(err.message);
     }
-  })
+  });
 
-  useEffect(()=>{ getChargesMutation.mutate(); }, [])
+  const deleteChargeMutation = useMutation<UnifiedResponse<undefined>, undefined, string>(deleteCharge);
+
+  useEffect(()=>{ getChargesMutation.mutate(); }, []);
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const key = msg + "-" + cash;
+    deleteChargeMutation.mutate(key);
+    setChargValue(initialData);
+  }
 
   const BoxStyle: React.CSSProperties = {
-    display:"flex",
-    justifyContent:"center",
-    textAlign:"center",
+    width: "18cm",
+    height: "6cm",
+    border: "2px solid black",
+    padding:"10px",
     fontSize:"22px",
-    alignItems:"center",
-    flexDirection:'row',
+    backgroundColor: "#D4F0F0",
   }
-  const BorderStyle: React.CSSProperties = { 
-    border:"3px solid black",
-    borderRight:"0px",
-    borderBottom:"0px",
-    width:"9cm",
-    height:"1cm",
-    display: "flex", 
-    alignItems: "center", 
-    justifyContent: "center",
+
+  const inDivStyle: React.CSSProperties = {
+    display:"flex", 
+    justifyItems:"center",
+    marginBottom:"20px",
   }
 
   return (
     <div style={ CommonStyle }>
         <h1 style={{  fontSize: "80px" }}>get Charge</h1>
-        <div style={{ ...BoxStyle, marginTop:"1cm", }}>
-          <div style={{ ...BorderStyle, backgroundColor:"#D4F0F0", }}><span>MSG</span></div>
-          <div style={{ ...BorderStyle, width:"14cm", backgroundColor:"#D4F0F0", }}><span >CASH</span></div>
-          <div style={{ borderLeft:"3px solid black", height:"1.08cm" }} />
-        </div>
         {msg !== "" && date !== "" ? (
           <div style={BoxStyle}>
-            <div style={BorderStyle}><span>{msg}</span></div>
-            <div style={{ ...BorderStyle, width:"14cm" }}><span>{cash}</span></div>
-            <div style={{ borderLeft:"3px solid black", height:"1.08cm" }} />
+            <div style={inDivStyle}>
+              <div style={{ marginLeft:"auto" }}>
+                만료: <span style={{ marginLeft:"20px", color:"blue" }}>{date}</span>
+              </div>
+            </div>
+            <div style={{ backgroundColor:"white", padding:"10px", width:"16.5cm", margin:"auto" }}>
+              <div style={inDivStyle}>
+                <span>입금 메세지:</span>
+                <span style={{ marginLeft:"auto" }}>{msg}</span>
+              </div>
+              <div style={inDivStyle}>
+                <span>금액:</span>
+                <span style={{ marginLeft:"auto" }}>{cash}</span>
+              </div>
+            </div>
+            <form onSubmit={onSubmitHandler} style={{ marginLeft: "auto", width: "5cm", marginTop:"20px"}}>
+              <button style={{ marginLeft: "auto", width: "100%", height:"30px" }}>삭제하기</button>
+            </form>
           </div>
-        ):null}
-        <div style={{ border:"3px solid black", borderBottom:"0px", width:"23cm", }}/>
+        ):(
+          <div style={{ textAlign:"center", fontSize:"22px", marginTop:"2cm" }}>
+            <span>거래내역이 존재하지 않습니다</span>
+          </div>
+        )}
     </div>
   )
 }
