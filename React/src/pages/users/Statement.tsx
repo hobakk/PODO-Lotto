42598f49d10cfb47cafcc131f4d9ac88ff4b674c
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-27 20:57:00 +0900

    #54 [fix] Statement.tsx

--- React/src/pages/users/Statement.tsx
+++ React/src/pages/users/Statement.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { CommonStyle } from '../../shared/Styles'
-import { getStatement } from '../../api/userApi'
+import { StatementResponse, getStatement } from '../../api/userApi'
 import { useMutation } from 'react-query'
 import { UnifiedResponse, Err } from '../../shared/TypeMenu';
 
 function Statement() {
-    const [value, setValue] = useState<{localDate: string, msg: string}[]>([]);
+    const [value, setValue] = useState<StatementResponse[]>([]);
 
-    const StateMnetMutation = useMutation<UnifiedResponse<{localDate: string, msg: string}[]>, any>(getStatement, {
+    const StateMnetMutation = useMutation<UnifiedResponse<StatementResponse[]>, any>(getStatement, {
         onSuccess: (res)=>{
             if (res.code === 200 && res.data) {
                 setValue(res.data);
@@ -21,45 +21,47 @@ function Statement() {
     useEffect(()=>{
         if  (value.length === 0)
         StateMnetMutation.mutate();
-    }, [])
+    }, []);
 
-    const PStyle: React.CSSProperties = {
+    useEffect(()=>console.log(value), [value]);
+
+    const TableStyle: React.CSSProperties = {
         border: "2px solid black",
-        borderBottom: "0px", 
-        width: "4cm", 
+        borderBottom: "0px",
+        borderRight: "0px",
+        width: "13%", 
         height: "1cm",
         display: "flex",
         justifyContent: "center",
         flexDirection: "column",
+        textAlign: "center",
     }
 
   return (
     <div style={ CommonStyle }>
         <h1 style={{  fontSize: "80px" }}>Statement</h1>
         <div>
-            <div style={{ display: "flex", fontSize:"22px", backgroundColor:"#D4F0F0"}}>
-                <span style={{...PStyle, textAlign: "center", borderRight: "0px", }}>DATE</span>
-                <span style={{...PStyle, width: "20cm"}}>
-                    <p style={{ marginLeft: "15px", textAlign:"center" }}>MSG</p>
-                </span>
+            <div style={{ display: "flex", fontSize:"22px", backgroundColor:"#D4F0F0", width:"40cm"}}>
+                <div style={TableStyle}>Date</div>
+                <div style={{ ...TableStyle, width: "34%", }}>Subject</div>
+                <div style={TableStyle}>Cash</div>
+                <div style={{ ...TableStyle, width: "50%", borderRight:"2px solid black" }}>Message</div>
             </div>
             {value.length !== 0 && (
                 value.map((item, index)=>{
                     return (
-                        <div key={item.localDate + index} style={{ fontSize: "20px"}}>
-                            <div style={{ display: "flex"}}>
-                                <span style={{...PStyle, textAlign: "center", borderRight: "0px", }}>
-                                    {item.localDate}
-                                </span>
-                                <span style={{...PStyle, width: "20cm"}}>
-                                    <p style={{ marginLeft: "15px" }}>{item.msg}</p>
-                                </span>
-                            </div>
+                        <div key={item.localDate + index} style={{ display: "flex", fontSize: "20px", width:"40cm"}}>
+                            <span style={TableStyle}>{item.localDate}</span>
+                            <span style={{ ...TableStyle, width: "34%", }}>{item.subject}</span>
+                            <span style={TableStyle}>{item.cash}</span>
+                            <span style={{ ...TableStyle, width:"50%", borderRight:"2px solid black" }}>
+                                {item.msg}
+                            </span>
                         </div>   
                     )
                 })
             )}
-            <div style={{ borderTop: "2px solid black", width: "24.1cm"}} />
+            <div style={{ borderTop: "2px solid black", width:"40cm"}} />
             {value.length === 0 && (
                 <div style={{ textAlign:"center", fontSize:"22px", marginTop:"2cm" }}>
                     <span>거래내역이 존재하지 않습니다</span>
