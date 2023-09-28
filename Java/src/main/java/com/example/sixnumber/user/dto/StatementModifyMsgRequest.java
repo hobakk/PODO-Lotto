66f8a10a58daeceaa413d2da70d60a54d30b1f6c
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-28 15:05:12 +0900

    [fix] StatementModifyMsgRequest @Builder

--- Java/src/main/java/com/example/sixnumber/user/dto/StatementModifyMsgRequest.java
+++ Java/src/main/java/com/example/sixnumber/user/dto/StatementModifyMsgRequest.java
@@ -1,10 +1,14 @@
 package com.example.sixnumber.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StatementModifyMsgRequest {
 	private Long statementId;
 	private String msg;
