85f2f209d4b8234e572a6430b010b13714841fa4
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-29 18:01:54 +0900

    [fix] SixNumberService 개선

--- Java/src/main/java/com/example/sixnumber/lotto/service/SixNumberService.java
+++ Java/src/main/java/com/example/sixnumber/lotto/service/SixNumberService.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -62,15 +63,15 @@ public class SixNumberService {
 				set.add(randomNum);
 			}
 
-			List<Integer> numList = new ArrayList<>(set);
-			Collections.sort(numList);
+			String result = new ArrayList<>(set).stream()
+				.sorted()
+				.map(Objects::toString)
+				.collect(Collectors.joining(" "));
 
-			String result = numList.stream().map(Object::toString).collect(Collectors.joining(" "));
 			topNumbers.add(result);
 		}
 
-		SixNumber sixNumber = new SixNumber(user, LocalDateTime.now(), topNumbers);
-		sixNumberRepository.save(sixNumber);
+		sixNumberRepository.save(new SixNumber(user, LocalDateTime.now(), topNumbers));
 		saveMainLottoList(topNumbers);
 		return UnifiedResponse.ok("요청 성공", topNumbers);
 	}
@@ -124,8 +125,7 @@ public class SixNumberService {
 			Thread.currentThread().interrupt();
 		}
 
-		SixNumber sixNumber = new SixNumber(user, LocalDateTime.now(), topNumbers);
-		sixNumberRepository.save(sixNumber);
+		sixNumberRepository.save(new SixNumber(user, LocalDateTime.now(), topNumbers));
 		saveMainLottoList(topNumbers);
 		return UnifiedResponse.ok("요청 성공", topNumbers);
 	}
