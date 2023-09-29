a1f9c4c875247285862042073059d1243e74798e
hobakk <117063625+hobakk@users.noreply.github.com>
2023-09-29 21:21:26 +0900

    [feat] StatementRepository findByIdAndAfterLastMonth

--- Java/src/main/java/com/example/sixnumber/user/repository/StatementRepository.java
+++ Java/src/main/java/com/example/sixnumber/user/repository/StatementRepository.java
@@ -1,8 +1,18 @@
 package com.example.sixnumber.user.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sixnumber.user.entity.Statement;
 
 public interface StatementRepository extends JpaRepository<Statement, Long> {
+	@Query("SELECT s FROM Statement s WHERE s.id = :statementId AND s.localDate >= :lastMonth")
+	Optional<Statement> findByIdAndAfterLastMonth(
+		@Param("statementId") Long statementId,
+		@Param("lastMonth") LocalDate lastMonth
+	);
 }
