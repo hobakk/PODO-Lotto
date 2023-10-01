c809c4a9df4559d03336d6fe3ab351d6c1eb498d
hobakk <117063625+hobakk@users.noreply.github.com>
2023-10-01 13:46:46 +0900

    [feat] FindPasswordRequest

---
+++ Java/src/main/java/com/example/sixnumber/user/dto/FindPasswordRequest.java
@@ -0,0 +1,17 @@
+package com.example.sixnumber.user.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FindPasswordRequest {
+	private String email;
+	@NotBlank(message = "비밀번호를 입력하세요.")
+	@Pattern(regexp = "^[A-Za-z0-9~!@#$%^&*=,.?]{8,60}$",
+		message = "비밀번호는 대소문자, 숫자, 특수문자 범주 안에 최소 8자 최대 60자 형식에 맞게 작성해주세요")
+	private String password;
+}
