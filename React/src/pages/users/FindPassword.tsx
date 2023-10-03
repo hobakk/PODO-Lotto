ae7829fbbea302e4a955c0ca2c9b9dfc4d839ab7
hobakk <117063625+hobakk@users.noreply.github.com>
2023-10-03 21:29:12 +0900

    [fix] FindPassword.tsx EmailAuthentication 적용

--- React/src/pages/users/FindPassword.tsx
+++ React/src/pages/users/FindPassword.tsx
@@ -4,10 +4,13 @@ import { useMutation } from 'react-query';
 import { findPassword } from '../../api/userApi';
 import { useNavigate } from 'react-router-dom';
 import { Err } from '../../shared/TypeMenu';
+import EmailAuthentication from '../../components/EmailAuthentication';
 
 function FindPassword() {
     const navigate = useNavigate();
     const [inputValue, setInputValue] = useState<{[name: string]: string}>({});
+    const [email, setEmail] = useState<string>("");
+    const [isCorrectAuth, setIsCorrectAuth] = useState<boolean>(false);
 
     const findPasswordMutation = useMutation(findPassword, {
         onSuccess: (res)=>{
@@ -29,38 +32,47 @@ function FindPassword() {
     const submitHandler =  (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (inputValue["chackPassword"] === inputValue["password"]) {
-            findPasswordMutation.mutate(inputValue["password"]);
+            findPasswordMutation.mutate({ email:email, password:inputValue["password"]});
         } else alert("비밀번호가 일치하지 않습니다")
     }
 
     return (
         <div style={CommonStyle}>
-             <h1 style={{  fontSize: "60px" }}>비밀번호 찾기</h1>
-            <form onSubmit={submitHandler}>
-                <div style={MsgAndInput}>
-                    <span>비밀번호:</span>
-                    <InputBox 
-                        type='password' 
-                        name="password" 
-                        placeholder='********'
-                        onChange={onChangeHandler} 
+            <h1 style={{  fontSize: "60px", marginBottom:"3cm" }}>비밀번호 찾기</h1>
+            <div style={{ fontSize: "30px", display: "flex", flexDirection: "column", width: "15cm" }}>
+                {isCorrectAuth ? (
+                    <form onSubmit={submitHandler}>
+                        <div style={MsgAndInput}>
+                            <span>비밀번호:</span>
+                            <InputBox 
+                                type='password' 
+                                name="password" 
+                                placeholder='********'
+                                onChange={onChangeHandler} 
+                            />
+                        </div>
+                        <div style={MsgAndInput}>
+                            <span>비밀번호 재확인:</span>
+                            <InputBox 
+                                type='password' 
+                                name="chackPassword" 
+                                placeholder='********'
+                                onChange={onChangeHandler} 
+                            />
+                        </div>
+                        <div style={{ display:"flex", marginLeft: "auto", width: "100%",}}>
+                            <button style={{ width: "7.2cm", height: "30px", marginLeft:"auto", }}>비밀번호 설정</button>
+                        </div>
+                    </form>
+                ):(
+                    <EmailAuthentication 
+                        isCorrectAuth={isCorrectAuth}
+                        setIsCorrectAuth={setIsCorrectAuth}
+                        email={email}
+                        setEmail={setEmail}
                     />
-                </div>
-                <div style={MsgAndInput}>
-                    <span>비밀번호 재확인:</span>
-                    <InputBox 
-                        type='password' 
-                        name="chackPassword" 
-                        placeholder='********'
-                        onChange={onChangeHandler} 
-                    />
-                </div>
-                <div style={{ display:"flex", marginLeft: "auto", width: "100%",}}>
-                    <button style={{ width: "7.2cm", height: "30px", marginLeft:"auto", }}>비밀번호 설정</button>
-                </div>
-            </form>
-           
-            
+                )}
+            </div>
         </div>
     )
 }
