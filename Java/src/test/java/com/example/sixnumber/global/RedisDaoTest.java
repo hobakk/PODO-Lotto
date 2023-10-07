01063131d425b05702f83b82baac2ee561081175
hobakk <117063625+hobakk@users.noreply.github.com>
2023-10-07 16:52:52 +0900

    [test] RedisDaoTest

--- Java/src/test/java/com/example/sixnumber/global/RedisDaoTest.java
+++ Java/src/test/java/com/example/sixnumber/global/RedisDaoTest.java
@@ -2,17 +2,13 @@ package com.example.sixnumber.global;
 
 import static org.mockito.Mockito.*;
 
-import java.util.Set;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import com.example.sixnumber.global.util.RedisDao;
 
@@ -23,37 +19,15 @@ public class RedisDaoTest {
 
 	@Mock
 	private RedisTemplate<String, String> redisTemplate;
+	@Mock
+	private ValueOperations<String, String> valueOperations;
 
-	// private ValueOperations<String, String> valueOperations;
-	private ListOperations<String, String> listOperations;
 	String key;
 
 	@BeforeEach
 	public void setup() {
 		key = "some_key";
-		// valueOperations = mock(ValueOperations.class);
-		listOperations = mock(ListOperations.class);
+		when(redisTemplate.opsForValue()).thenReturn(valueOperations);
 	}
 
-	@Test
-	void multiGet() {
-		Set<String> value = mock(Set.class);
-		when(value.size()).thenReturn(0);
-
-		when(redisTemplate.keys(anyString())).thenReturn(value);
-
-		Assertions.assertThrows(IllegalArgumentException.class, ()->redisDao.multiGet(anyString()));
-
-		verify(redisTemplate).keys(anyString());
-	}
-
-	// @Test
-	// void getValuesList() {
-	// 	when(redisTemplate.opsForList()).thenReturn(listOperations);
-	// 	when(listOperations.range(anyString(), anyLong(), anyLong())).thenReturn(new ArrayList<>());
-	//
-	// 	Assertions.assertThrows(IllegalArgumentException.class, ()->redisDao.getValuesList(key));
-	//
-	// 	verify(listOperations).range(anyString(), anyLong(), anyLong());
-	// }
 }
