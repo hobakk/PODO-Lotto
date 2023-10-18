b90cef215c9de67429bb327be132ac97ad055aeb
hobakk <hobakk12@gmail.com>
2023-10-18 12:47:32 +0900

    [fix] WebConfig url

--- Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
+++ Java/src/main/java/com/example/sixnumber/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.sixnumber.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+	@Value("${spring.redis.host}")
+	private String ec2Url;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins(ec2Url + ":3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 			.allowedHeaders("*")
 			.exposedHeaders("Authorization")
