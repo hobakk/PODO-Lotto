e9310789cd35d2374ae2aebd4f4685cf4e6df112
hobakk <hobakk12@gmail.com>
2023-10-22 23:24:51 +0900

    [fix] nginx default.conf

--- React/src/api/config.js
+++ React/src/api/config.js
@@ -4,7 +4,6 @@ const url = `${process.env.REACT_APP_BASE_URL}`
 
 const dontLogin = axios.create({
     headers: {
-        'Access-Control-Allow-Origin': `${url}:8080`,
         "Content-Type": 'application/json',	// 서버 domain, httpOnly 설정 때문에 추가
     },
     baseURL: `${url}/api`,
@@ -12,9 +11,9 @@ const dontLogin = axios.create({
 })
 
 const api = axios.create({
-    headers: {
-        'Access-Control-Allow-Origin': `${url}:8080`,
-    },
+    // headers: {
+    //     'Access-Control-Allow-Origin': `${url}:8080`,
+    // },
     baseURL: `${url}/api`,
     withCredentials: true,
 })
