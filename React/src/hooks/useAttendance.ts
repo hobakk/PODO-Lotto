c83c0fe9a7641db9bb059fe7433b5c90fb73bf7e
hobakk <hobakk12@gmail.com>
2023-11-01 19:19:48 +0900

    [fix] useAttendance.ts 사용자 갱신

--- React/src/hooks/useAttendance.ts
+++ React/src/hooks/useAttendance.ts
@@ -1,11 +1,14 @@
 import { useMutation } from 'react-query';
 import { UnifiedResponse } from '../shared/TypeMenu';
 import { attendance } from '../api/userApi';
+import useUserInfo from './useUserInfo';
 
 function useAttendance() {
+    const { getCashAndNickname } = useUserInfo();
 
     const attendanceMutation = useMutation<UnifiedResponse<undefined>>(attendance, {
         onSuccess: (res) => {
+            getCashAndNickname.mutate();
             alert(res.msg);
         },
         onError: (err)=>{
