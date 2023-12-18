da23ba56b14b0a3fa4c05ae936d71a0d83c51595
hobakk <hobakk12@gmail.com>
2023-12-18 21:14:42 +0900

    #61 [feat] CommentController setComment

---
+++ Java/src/main/java/com/example/sixnumber/board/controller/CommentController.java
@@ -0,0 +1,31 @@
+package com.example.sixnumber.board.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sixnumber.board.dto.CommentRequest;
+import com.example.sixnumber.board.service.CommentService;
+import com.example.sixnumber.global.dto.UnifiedResponse;
+import com.example.sixnumber.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+
+	private final CommentService commentService;
+
+	@PostMapping("/{boardId}")
+	public ResponseEntity<UnifiedResponse<?>> setComment(
+		@RequestBody CommentRequest request,
+		@AuthenticationPrincipal User user
+	) {
+		return ResponseEntity.ok(commentService.setComment(user, request));
+	}
+}
