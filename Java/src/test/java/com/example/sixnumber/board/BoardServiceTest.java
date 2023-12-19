a7782f41dad459d55f2fe02e83351d97edd60f37
hobakk <hobakk12@gmail.com>
2023-12-20 00:40:21 +0900

    [test] BoardServiceTest setBoard_success

---
+++ Java/src/test/java/com/example/sixnumber/board/BoardServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.sixnumber.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.sixnumber.board.entity.Board;
+import com.example.sixnumber.board.repository.BoardRepository;
+import com.example.sixnumber.board.service.BoardService;
+import com.example.sixnumber.board.type.BoardStatus;
+import com.example.sixnumber.fixture.TestDataFactory;
+import com.example.sixnumber.fixture.TestUtil;
+import com.example.sixnumber.global.dto.UnifiedResponse;
+import com.example.sixnumber.global.util.Manager;
+import com.example.sixnumber.user.entity.User;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardServiceTest {
+	@InjectMocks
+	private BoardService boardService;
+
+	@Mock
+	private BoardRepository boardRepository;
+
+	@Mock
+	private Manager manager;
+	private User saveUser;
+
+	@BeforeEach
+	public void setup() {
+		saveUser = TestDataFactory.user();
+	}
+
+	@Test
+	void setBoard_success() {
+		when(boardRepository.findAllByUserIdAndStatus(anyLong(), any(BoardStatus.class)))
+			.thenReturn(List.of(TestDataFactory.board()));
+
+		UnifiedResponse<?> response = boardService.setBoard(TestDataFactory.boardRequest(), saveUser);
+
+		verify(boardRepository).findAllByUserIdAndStatus(anyLong(), any(BoardStatus.class));
+		verify(boardRepository).save(any(Board.class));
+		TestUtil.UnifiedResponseEquals(response, 200, "생성 완료");
+	}
+}
