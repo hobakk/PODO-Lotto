2b048a0b97f12cc67edeb2811eb90adef27d92b8
hobakk <hobakk12@gmail.com>
2023-12-21 21:07:16 +0900

    [fix] CommentServiceTest setup 추가

---
+++ Java/src/test/java/com/example/sixnumber/board/CommentServiceTest.java
@@ -0,0 +1,62 @@
+package com.example.sixnumber.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.sixnumber.board.dto.CommentRequest;
+import com.example.sixnumber.board.entity.Comment;
+import com.example.sixnumber.board.repository.BoardRepository;
+import com.example.sixnumber.board.repository.CommentRepository;
+import com.example.sixnumber.board.service.CommentService;
+import com.example.sixnumber.fixture.TestDataFactory;
+import com.example.sixnumber.fixture.TestUtil;
+import com.example.sixnumber.global.dto.UnifiedResponse;
+import com.example.sixnumber.global.util.Manager;
+import com.example.sixnumber.user.entity.User;
+
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+	@InjectMocks
+	private CommentService commentService;
+
+	@Mock
+	private BoardRepository boardRepository;
+
+	@Mock
+	private CommentRepository commentRepository;
+
+	@Mock
+	private Manager manager;
+	private User saveUser;
+	private CommentRequest commentRequest;
+
+	@BeforeEach
+	public void setup() {
+		saveUser = TestDataFactory.user();
+		commentRequest = TestDataFactory.commentRequest();
+	}
+
+	@Test
+	void setComment_isNotAdmin() {
+		when(manager.isAdmin(any(User.class))).thenReturn(false);
+		when(boardRepository.findByIdAndCommentEnabled(anyLong(), anyBoolean()))
+			.thenReturn(Optional.of(TestDataFactory.board()));
+
+		UnifiedResponse<?> response = commentService.setComment(saveUser, commentRequest);
+
+		verify(manager).isAdmin(any(User.class));
+		verify(boardRepository).findByIdAndCommentEnabled(anyLong(), anyBoolean());
+		verify(commentRepository).save(any(Comment.class));
+		TestUtil.UnifiedResponseEquals(response, 200, "댓글 작성 완료");
+	}
+}
