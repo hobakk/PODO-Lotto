df9620d939c41c42363976106b05aff4f381652d
hobakk <hobakk12@gmail.com>
2023-12-22 19:22:27 +0900

    [feat] commentApi.ts setComment

--- React/src/api/commentApi.ts
+++ React/src/api/commentApi.ts
@@ -5,3 +5,17 @@ export type CommentResponse = {
     nickname: string,
     message: string
 }
+
+export type CommentRequest = {
+    id: number,
+    message: string
+}
+
+export const setComment = async (req: CommentRequest): Promise<UnifiedResponse<undefined>> => {
+    try {
+        const { data } = await api.post("/comment", req);
+        return data;  
+    } catch (error: any) {
+        throw error.data;
+    }
+}
