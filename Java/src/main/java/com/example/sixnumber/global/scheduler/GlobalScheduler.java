a24bc1486bbe2ee4e8e6e2467155bae79d8babde
hobakk <hobakk12@gmail.com>
2024-02-18 22:16:46 +0900

    #55 [refactor] 월별 통계 생성

--- Java/src/main/java/com/example/sixnumber/global/scheduler/GlobalScheduler.java
+++ Java/src/main/java/com/example/sixnumber/global/scheduler/GlobalScheduler.java
@@ -3,12 +3,7 @@ package com.example.sixnumber.global.scheduler;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,11 +11,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.example.sixnumber.global.util.Manager;
-import com.example.sixnumber.lotto.entity.Lotto;
 import com.example.sixnumber.lotto.entity.WinNumber;
-import com.example.sixnumber.lotto.repository.LottoRepository;
-import com.example.sixnumber.lotto.repository.SixNumberRepository;
 import com.example.sixnumber.lotto.repository.WinNumberRepository;
+import com.example.sixnumber.lotto.service.LottoService;
 import com.example.sixnumber.lotto.service.WinNumberService;
 import com.example.sixnumber.user.entity.User;
 import com.example.sixnumber.user.repository.UserRepository;
@@ -35,8 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class GlobalScheduler {
 
 	private final UserRepository userRepository;
-	private final LottoRepository lottoRepository;
-	private final SixNumberRepository sixNumberRepository;
+	private final LottoService lottoService;
 	private final WinNumberRepository winNumberRepository;
 	private final WinNumberService winNumberService;
 	private final RedisTemplate<String, String> redisTemplate;
@@ -45,27 +37,7 @@ public class GlobalScheduler {
 	@Scheduled(cron = "0 0 11 ? * SUN")
 	public void findByTopNumberListForMonth() {
 		YearMonth lastMonth = YearMonth.now().minusMonths(1);
-		if (!lottoRepository.existsLottoByCreationDate(lastMonth)) {
-			Map<Integer, Integer> map = new HashMap<>();
-
-			sixNumberRepository.findAllByBuyDate(lastMonth.getYear(), lastMonth.getMonthValue()).forEach(sixNumber ->
-				sixNumber.getNumberList().forEach(sentence ->
-					Stream.of(sentence.split(" ")).forEach(topNumberStr -> {
-						int key = Integer.parseInt(topNumberStr);
-						map.put(key, map.getOrDefault(key, 0) + 1);
-					})
-				)
-			);
-
-			String result = manager.getTopNumbersAsString(map);
-
-			List<Integer> countList = IntStream.rangeClosed(1, 45)
-				.mapToObj(i -> map.getOrDefault(i, 0))
-				.collect(Collectors.toList());
-
-			Lotto lotto = new Lotto("Stats", "Scheduler", lastMonth, countList, result);
-			lottoRepository.save(lotto);
-		}
+		lottoService.createMonthlyReport(lastMonth.getYear(), lastMonth.getMonthValue());
 	}
 
 	@Scheduled(cron = "0 0 9 * * *")
--- Java/src/main/java/com/example/sixnumber/lotto/service/LottoService.java
+++ Java/src/main/java/com/example/sixnumber/lotto/service/LottoService.java
@@ -5,15 +5,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.sixnumber.global.dto.UnifiedResponse;
+import com.example.sixnumber.global.exception.CustomException;
+import com.example.sixnumber.global.exception.ErrorCode;
 import com.example.sixnumber.global.util.Manager;
 import com.example.sixnumber.lotto.dto.LottoResponse;
 import com.example.sixnumber.lotto.dto.YearMonthResponse;
+import com.example.sixnumber.lotto.entity.Lotto;
+import com.example.sixnumber.lotto.entity.SixNumber;
 import com.example.sixnumber.lotto.repository.LottoRepository;
+import com.example.sixnumber.lotto.repository.SixNumberRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -23,6 +32,7 @@ import lombok.AllArgsConstructor;
 public class LottoService {
 
 	private final LottoRepository lottoRepository;
+	private final SixNumberRepository sixNumberRepository;
 	private final Manager manager;
 
 	@Cacheable(cacheNames = "MainStats", key = "'all'")
@@ -56,5 +66,36 @@ public class LottoService {
 			.orElseThrow(() -> new IllegalArgumentException("해당 정보를 찾을 수 없습니다"));
 
 		return new YearMonthResponse(yearMonthList);
+	}
+
+	public UnifiedResponse<?> createMonthlyReport(int year, int month) {
+		YearMonth yearMonth = YearMonth.of(year, month);
+		if (lottoRepository.existsLottoByCreationDate(yearMonth)
+			|| yearMonth.equals(YearMonth.now())
+			|| yearMonth.isAfter(YearMonth.now())
+		) throw new IllegalArgumentException("이미 처리되었거나 잘못된 입력값 입니다");
+
+		Map<Integer, Integer> map = new HashMap<>();
+		List<SixNumber> sixNumberList = sixNumberRepository.findAllByBuyDate(year, month);
+		if (sixNumberList.isEmpty()) throw new CustomException(ErrorCode.NOT_FOUND);
+
+		sixNumberList.forEach(sixNumber -> sixNumber.getNumberList()
+			.forEach(sentence -> Stream.of(sentence.split(" "))
+				.forEach(topNumberStr -> {
+					int key = Integer.parseInt(topNumberStr);
+					map.put(key, map.getOrDefault(key, 0) + 1);
+				})
+			)
+		);
+
+		String result = manager.getTopNumbersAsString(map);
+
+		List<Integer> countList = IntStream.rangeClosed(1, 45)
+			.mapToObj(i -> map.getOrDefault(i, 0))
+			.collect(Collectors.toList());
+
+		Lotto lotto = new Lotto("Stats", "Scheduler", yearMonth, countList, result);
+		lottoRepository.save(lotto);
+		return UnifiedResponse.ok("월별 통계 생성완료");
 	}
 }
