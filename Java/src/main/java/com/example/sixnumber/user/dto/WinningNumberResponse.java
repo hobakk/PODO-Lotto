b9c0c0553c740255643025f6a151dfe167ef8bb9
hobakk <hobakk12@gmail.com>
2024-03-03 21:34:52 +0900

    #62 [feat] 당첨된 번호 Response

---
+++ Java/src/main/java/com/example/sixnumber/user/dto/WinningNumberResponse.java
@@ -0,0 +1,33 @@
+package com.example.sixnumber.user.dto;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+
+@Getter
+public class WinningNumberResponse {
+	private final List<Integer> numberList;
+	private static final Map<Integer, Integer> rankMap;
+	private final int rank;
+
+	static {
+		rankMap = new HashMap<>();
+		rankMap.put(3, 5);
+		rankMap.put(4, 4);
+		rankMap.put(5, 3);
+		rankMap.put(6, 1);
+		rankMap.put(7, 2);
+	}
+
+	public WinningNumberResponse(String numberSentence, int numberOfWins) {
+		this.numberList = Arrays.stream(numberSentence.split(" "))
+			.mapToInt(Integer::parseInt)
+			.boxed()
+			.collect(Collectors.toList());
+		this.rank = rankMap.get(numberOfWins);
+	}
+}
