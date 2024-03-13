8af9307244ee48bd3b83c727679e32462c595796
hobakk <hobakk12@gmail.com>
2024-03-14 00:52:43 +0900

    [test] FE: 모든 문의 확인 테스트

--- React/src/pages/admin/GetBoards.tsx
+++ React/src/pages/admin/GetBoards.tsx
@@ -2,13 +2,30 @@ import React, { useEffect, useState } from 'react'
 import { CommonStyle, TitleStyle } from '../../shared/Styles'
 import { useMutation } from 'react-query'
 import { UnifiedResponse } from '../../shared/TypeMenu'
+import { PageBoardsRes, getAllBoardsByStatus } from '../../api/boardApi';
+import SelectStatus from '../../components/SelectStatus';
 
 function GetBoard() {
-    const [value, setValue] = useState<string | undefined>();
+    const [selectedStatus, setSelectedStatus] = useState<string>("");
+    const [value, setValue] = useState<PageBoardsRes>();
+
+    const getAllBoardsMutation = useMutation(getAllBoardsByStatus, {
+        onSuccess: (res)=>{
+            if (res.code === 200 && res.data) setValue(res.data);
+        },
+        onError: (err)=>{
+            console.log(err);
+        }
+    })
+
+    useEffect(()=>{
+        if (selectedStatus !== "") getAllBoardsMutation.mutate(selectedStatus);
+    }, [selectedStatus])
 
     return (
         <div style={CommonStyle}>
             <h1 style={ TitleStyle }>모든 문의 확인</h1>
+            <SelectStatus onChange={(e)=>{setSelectedStatus(e)}} />
         </div>
     )
 }
