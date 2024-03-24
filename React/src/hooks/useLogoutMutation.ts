146d03025025068cdb1c262924a20324346b13eb
hobakk <hobakk12@gmail.com>
2024-03-24 14:20:36 +0900

    [refactor] FE: 로그아웃 훅

--- React/src/hooks/useLogoutMutation.ts
+++ React/src/hooks/useLogoutMutation.ts
@@ -3,16 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { logout } from '../api/userApi';
 import { UnifiedResponse } from '../shared/TypeMenu';
 import { persistor } from '../config/configStore';
+import useDeleteCookie from './useDeleteCookie';
 
 function useLogoutMutation() {
     const navigate = useNavigate();
     const purge = async () => { await persistor.purge(); }
+    const deleteCookieMutation = useDeleteCookie();
 
     const logoutMutation = useMutation<UnifiedResponse<undefined>>(logout, {
         onSuccess: (res) => {
             if (res.code === 200) {
                 purge();
-                navigate("/");
+                deleteCookieMutation.mutate();
             }
         }
     })
