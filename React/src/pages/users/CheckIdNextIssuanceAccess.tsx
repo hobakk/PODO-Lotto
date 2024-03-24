12a5d77fa26594208cc3d5ca73876f88ec652691
hobakk <hobakk12@gmail.com>
2024-03-24 14:21:19 +0900

    [feat] FE: id확인 후 access재발급 page

--- React/src/pages/users/CheckIdNextIssuanceAccess.tsx
+++ React/src/pages/users/CheckIdNextIssuanceAccess.tsx
@@ -6,26 +6,37 @@ import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { RootState, persistor } from '../../config/configStore';
 import { Err, UnifiedResponse } from '../../shared/TypeMenu';
+import useDeleteCookie from '../../hooks/useDeleteCookie';
 
 function CheckIdNextIssuanceAccess() {
     const userIf = useSelector((state: RootState)=>state.userIf);
+    const [id, setid] = useState<number>(-1);
     const navigate = useNavigate();
     const purge = async () => { await persistor.purge(); }
+    const deleteCookieMutation = useDeleteCookie();
 
-    const mutation = useMutation<UnifiedResponse<undefined>, Err, string>(checkIdNextIssuanceAccess, {
+    const mutation = useMutation<UnifiedResponse<undefined>, Err, number>(checkIdNextIssuanceAccess, {
         onSuccess: (res)=>{
             if (res.code === 200) navigate("/");
         },
         onError: (err) =>{
             alert("다시 로그인 해주세요");
             purge();
-            // 쿠키 삭제하는 api 추가 필
+            deleteCookieMutation.mutate();
         }
     })
 
     useEffect(()=>{
-        if (userIf.userId) mutation.mutate(String(userIf.userId));
+        setid(userIf.userId);
     }, [userIf])
+
+    useEffect(()=>{
+        console.log(id)
+        if (id === 0) {
+            purge();
+            deleteCookieMutation.mutate();  
+        } else if (id > 0) mutation.mutate(id);
+    }, [id])
 
     return (
         <div>
