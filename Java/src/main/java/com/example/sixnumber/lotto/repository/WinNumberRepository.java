06ca78241ccc4c1fe64fbde4ace85d5976efafdc
hobakk <hobakk12@gmail.com>
2024-03-31 15:22:04 +0900

    [feat] 당첨번호 회차 조회 메서드

--- Java/src/main/java/com/example/sixnumber/lotto/repository/WinNumberRepository.java
+++ Java/src/main/java/com/example/sixnumber/lotto/repository/WinNumberRepository.java
@@ -2,6 +2,7 @@ package com.example.sixnumber.lotto.repository;
 
 import org.springframework.data.domain.Pageable;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,6 @@ public interface WinNumberRepository extends JpaRepository<WinNumber, Long> {
 
 	@Query("SELECT w FROM WinNumber w ORDER BY w.time DESC")
 	List<WinNumber> findTopByTime(Pageable pageable);
+
+	Optional<WinNumber> findByTime(int time);
 }
